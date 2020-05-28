baa077e8c8d5e58a621cc1d5ea686cd559ca57a7
Abhishek Chakravarti <abhishek@taranjali.org>
2020-05-28 08:38:50 +0530

    Develop exception handling mechanism
    The exception handling mechanism has been developed with the
    following interface:
      * ag_erno enumeration
      * ag_exception struct
      * ag_exception_handler callback
      * ag_exception_message() function
      * ag_exception_handler_get() function
      * ag_exception_handler_set() function
      * ag_assert() macro
      * ag_require() macro
    
    Initially, I had planned simply to implement the ag_assert() and
    ag_require() macros without any form of exception handling, but a
    bit of thought showed that it was doable without much effort.
    
    The exception handling mechansim works by allowing the client code
    to define an exception handler (through the ag_exception_handler
    callback) which is then subsequently used by the Argent Library. In
    case no exception handler is defined by the client code, then a
    default handler is used instead.
    
    There is an important distinction between ag_assert() and
    ag_require(), corresponding to the difference between an assertion
    and an exception. An assertion is used to ensure that a condition
    which should never occur (resulting from a programming error) is
    caught, whereas an exception is raised to indicate a condition that
    may go wrong at runtime, such as heap failure. Accordingly,
    ag_assert() checks may be disabled at runtime once we are sure that
    there is no programming error, but runtime checks through
    ag_require() are never disabled.
    
    Additionally, I have updated the Developer Manual references for the
    compiler extensions module.
    
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/api.h
+++ src/api.h
@@ -7,7 +7,7 @@
  * Argent Library
  * Copyright (c) 2020 Abhishek Chakravarti <abhishek@taranjali.org>
  *
- * This file is part of the Argent Library. It defines application programming
+ * This file is part of the Argent Library. It declares application programming
  * interface of the Argent Library.
  *
  * The contents of this file are released under the GPLv3 License. See the
@@ -19,6 +19,11 @@
  ******************************************************************************/
 
 
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
 
 /*******************************************************************************
  *                             COMPILER EXTENSIONS
@@ -26,32 +31,8 @@
 
 
 /*
- *      The ag_likely() macro provides a branch prediction hint that a predicate
- *      is likely to be true [DM:??]
- */
-#if (defined __GNUC__ || defined __clang__)
-#   define ag_likely(p) (__builtin_expect(!!(p), 1))
-#else
-#   define ag_likely(p) (p)
-#   warning "[!] ag_likely() not supported by current compiler"
-#endif
-
-
-/*
- *      The ag_unlikely() macro provides a branch prediction hint that a 
- *      predicate is likely to be false [DM:??]
- */
-#if (defined __GNUC__ || defined __clang__)
-#   define ag_unlikely(p) (__builtin_expect(!!(p), 0))
-#else
-#   define ag_unlikely(p) (p)
-#   warning "[!] ag_unlikely not supported by current compiler"
-#endif
-
-
-/*
  *      The ag_pure macro hints that a given function is pure, having no side
- *      effects [DM:??]
+ *      effects [DM:2.1]
  */
 #if (defined __GNUC__ || defined __clang__)
 #   define ag_pure __attribute__((pure))
@@ -63,7 +44,7 @@
 
 /*
  *      The ag_hot macro hints that a given function is called frequently
- *      [DM:??].
+ *      [DM:2.2].
  */
 #if (defined __GNUC__ || defined __clang__)
 #   define ag_hot __attribute__((hot))
@@ -75,7 +56,7 @@
 
 /*
  *      The ag_cold macro hints that a given function is called infrequently
- *      [DM:??].
+ *      [DM:2.3].
  */
 #if (defined __GNUC__ || defined __clang__)
 #   define ag_cold __attribute__((cold))
@@ -86,8 +67,32 @@
 
 
 /*
+ *      The ag_likely() macro provides a branch prediction hint that a predicate
+ *      is likely to be true [DM:2.4]
+ */
+#if (defined __GNUC__ || defined __clang__)
+#   define ag_likely(p) (__builtin_expect(!!(p), 1))
+#else
+#   define ag_likely(p) (p)
+#   warning "[!] ag_likely() not supported by current compiler"
+#endif
+
+
+/*
+ *      The ag_unlikely() macro provides a branch prediction hint that a 
+ *      predicate is likely to be false [DM:2.5]
+ */
+#if (defined __GNUC__ || defined __clang__)
+#   define ag_unlikely(p) (__builtin_expect(!!(p), 0))
+#else
+#   define ag_unlikely(p) (p)
+#   warning "[!] ag_unlikely not supported by current compiler"
+#endif
+
+
+/*
  *      The ag_threadlocal macro hints that a given variable has thread local
- *      storage [DM:??].
+ *      storage [DM:2.6].
  */
 #if (defined __STDC_VERSION__ && __STDC_VERSION__ >= 201112L)
 #   include <threads.h>
@@ -98,4 +103,95 @@
 #    define ag_threadlocal
 #    warning "[!] ag_threadlocal not supported by current compiler"
 #endif
+
+
+
+
+/*******************************************************************************
+ *                             EXCEPTION HANDLING
+ */
+
+
+/*
+ *      The ag_erno enumeration holds the error codes of the runtime exceptions 
+ *      raised by the Argent Library [DM:??].
+ */
+enum ag_erno {
+    AG_ERNO_NULL = 0x0,
+    AG_ERNO_LEN
+};
+
+
+/*
+ *      The ag_exception struct represents the details of a runtime exception
+ *      raised by the Argent Library [DM:??].
+ */
+struct ag_exception {
+    enum ag_erno erno;
+    const char *func;
+    const char *file;
+    size_t line;
+};
+
+
+/*
+ *      The ag_exception_handler callback function allows client code to handle
+ *      runtime exceptions [DM:??].
+ */
+typedef void (ag_exception_handler)(const struct ag_exception *ex, void *opt);
+
+
+/*
+ *      The ag_exception_message() interface function gets the human-readable
+ *      error message corresponding to an error code [DM:??].
+ */
+extern const char *ag_exception_message(enum ag_erno erno);
+
+
+/*
+ *      The ag_exception_handler_get() interface function gets the thread-local
+ *      exception handler [DM:??].
+ */
+extern ag_exception_handler *ag_exception_handler_get(void);
+
+
+/*
+ *      The ag_exception_handler_set() interface function sets the thread-local
+ *      exception handler [DM:??].
+ */
+extern void ag_exception_handler_set(ag_exception_handler *eh);
+
+
+/*
+ *      The ag_assert() macro performs a runtime assertion that a given 
+ *      predicate is true [DM:??].
+ */
+#if (!defined NDEBUG)
+#   define ag_assert(p) do {                                   \
+        if (ag_unlikely (!(p))) {                              \
+            printf("[!] assertion failed: %s [%s(), %s:%d]\n", \
+                    #p, __func__, __FILE__, __LINE__);         \
+            abort();                                           \
+        }                                                      \
+    } while (0)
+#else
+#   define ag_assert(p)
+#endif
+
+
+/*
+ *      The ag_require() macro ensures that a given predicate is true, raising
+ *      an exception otherwise [DM:??].
+ */
+#define ag_require(p, e, opt) do {           \
+    if (ag_unlikely (!(p))) {                \
+        struct ag_exception ex = {           \
+            .erno = e,                       \
+            .func = __func__,                \
+            .file = __FILE__,                \
+            .line = __LINE__                 \
+        };                                   \
+        ag_exception_handler_get()(ex, opt); \
+    }                                        \
+} while (0)
 
---
+++ src/exception.c
@@ -0,0 +1,115 @@
+/*******************************************************************************
+ *                        __   ____   ___  ____  __ _  ____ 
+ *                       / _\ (  _ \ / __)(  __)(  ( \(_  _)
+ *                      /    \ )   /( (_ \ ) _) /    /  )(  
+ *                      \_/\_/(__\_) \___/(____)\_)__) (__)                     
+ *
+ * Argent Library
+ * Copyright (c) 2020 Abhishek Chakravarti <abhishek@taranjali.org>
+ *
+ * This file is part of the Argent Library. It implements the exception handling
+ * interface of the Argent Library.
+ *
+ * The contents of this file are released under the GPLv3 License. See the
+ * accompanying LICENSE file or the generated Developer Manual (section I:?) for 
+ * complete licensing details.
+ *
+ * BY CONTINUING TO USE AND/OR DISTRIBUTE THIS FILE, YOU ACKNOWLEDGE THAT YOU
+ * HAVE UNDERSTOOD THESE LICENSE TERMS AND ACCEPT TO BE LEGALLY BOUND BY THEM.
+ ******************************************************************************/
+
+
+#include "./api.h"
+
+
+
+
+/*******************************************************************************
+ *                             HELPER PROTOTYPES
+ */
+
+
+/*
+ *      The eh_default() helper function is the default exception handler used
+ *      in case none is supplied by the client code [DM:??].
+ */
+static void eh_default(const struct ag_exception *ex, void *opt);
+
+
+
+
+/*******************************************************************************
+ *                              GLOBAL VARIABLES
+ */
+
+
+/*
+ *      The g_eh thread-local callback holds the exception handler that is to be
+ *      invoked in case of an exception [DM:??].
+ */
+static ag_threadlocal ag_exception_handler *g_eh = eh_default;
+
+
+/*
+ *      The g_emsg thread-local array stores the error messages corresponding to
+ *      each enum ag_erno enumerator [DM:??]
+ */
+static ag_threadlocal const char *g_emsg[] = {
+    "AG_ERNO_NULL: no error has occurred"
+};
+
+
+
+
+/*******************************************************************************
+ *                          INTERFACE IMPLEMENTATION
+ */
+
+
+/*
+ *      Implementation of the ag_exception_message() interface function [DM:??].
+ */
+extern const char *ag_exception_message(enum ag_erno erno)
+{
+    return ag_likely (erno >= AG_ERNO_NULL && erno < AG_ERNO_LEN) 
+            ? g_emsg[erno] : "[!] undefined error message";
+}
+
+
+/*
+ *      Implementation of the ag_exception_handler_get() interface function
+ *      [DM:??].
+ */
+extern ag_exception_handler *ag_exception_handler_get(void)
+{
+    return g_eh;
+}
+
+
+/*
+ *      Implementation of the ag_exception_handler_set() interface function
+ *      [DM:??].
+ */
+extern void ag_exception_handler_set(ag_exception_handler *eh)
+{
+    g_eh = ag_likely (eh) ? eh : eh_default;
+}
+
+
+
+
+/*******************************************************************************
+ *                           HELPER IMPLEMENTATION
+ */
+
+
+/*
+ *      Implementation of the eh_default() helper function [DM:??].
+ */
+static void eh_default(const struct ag_exception *ex, void *opt)
+{
+    printf("[!] unhandled exception: 0x%x [%s(), %s:%lu]\n%s\n", ex->erno, 
+            ex->func, ex->file, ex->line, ag_exception_message(ex->erno));
+    exit(EXIT_FAILURE);
+}
+
