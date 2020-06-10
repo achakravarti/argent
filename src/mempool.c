9017cc2c602ad972fd89025fb9f92cc57a43ca1e
Abhishek Chakravarti <abhishek@taranjali.org>
2020-06-10 09:37:01 +0530

    Implement ag_mempool_new()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/mempool.c
+++ src/mempool.c
@@ -44,6 +44,25 @@ extern void *ag_mempool_new(size_t sz)
 }
 
 
+extern void *ag_mempool_new_aligned(size_t align, size_t sz)
+{
+    ag_assert (align && sz && !(align & (align - 1)) && !(sz % align));
+
+#if (defined __STDC_VERSION__ && __STDC_VERSION__ >= 201112L)
+    void *bfr = aligned_alloc(align, sz);
+    ag_require (bfr, AG_ERNO_MEMPOOL_NEW, NULL);
+#elif defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
+    void *bfr;
+    ag_require (!posix_memalign(&bfr, align, sz), AG_ERNO_MEMPOOL_NEW, NULL);
+#else
+#   error "[!!] ag_mempool_new_aligned() not supported by current host"
+#endif
+
+    memset(bfr, 0, sz);
+    return bfr;
+}
+
+
 /*
  *      Implementation of the ag_mempool_resize() interface function [DM:??].
  */
