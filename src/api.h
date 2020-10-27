c071141fe2f18864dcc125a086106b9648b84d4b
Abhishek Chakravarti <abhishek@taranjali.org>
2020-10-27 18:27:44 +0530

    Declare ag_url_t interface
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/api.h
+++ src/api.h
@@ -938,6 +938,85 @@ enum ag_http_mime {
 };
 
 
+typedef ag_object_t ag_url_t;
+#define ag_url_smart_t ag_object_smart_t
+extern void ag_url_register(void);
+
+extern ag_url_t *ag_url_new(bool secure, const char *host, const char *port,
+        const char *path);
+
+inline ag_url_t *ag_url_copy(const ag_url_t *ctx)
+{
+    return ag_object_copy(ctx);
+}
+
+
+inline void ag_url_dispose(ag_url_t **ctx)
+{
+    ag_object_dispose(ctx);
+}
+
+inline size_t ag_url_typeid(const ag_url_t *ctx)
+{
+    return ag_object_type(ctx);
+}
+
+inline size_t ag_url_objid(const ag_url_t *ctx)
+{
+    return ag_object_id(ctx);
+}
+
+inline size_t ag_url_hash(const ag_url_t *ctx)
+{
+    return ag_object_hash(ctx);
+}
+
+inline size_t ag_url_sz(const ag_url_t *ctx)
+{
+    return ag_object_sz(ctx);
+}
+
+inline size_t ag_url_len(const ag_url_t *ctx)
+{
+    return ag_object_len(ctx);
+}
+
+inline bool ag_url_empty(const ag_url_t *ctx)
+{
+    return ag_object_empty(ctx);
+}
+
+inline enum ag_tristate ag_url_cmp(const ag_url_t *ctx, const ag_url_t *cmp)
+{
+    return ag_object_cmp(ctx, cmp);
+}
+
+inline bool ag_url_lt(const ag_url_t *ctx, const ag_url_t *cmp)
+{
+    return ag_object_lt(ctx, cmp);
+}
+
+inline bool ag_url_eq(const ag_url_t *ctx, const ag_url_t *cmp)
+{
+    return ag_object_eq(ctx, cmp);
+}
+
+inline bool ag_url_gt(const ag_url_t *ctx, const ag_url_t *cmp)
+{
+    return ag_object_gt(ctx, cmp);
+}
+
+inline const char *ag_url_str(const ag_url_t *ctx)
+{
+    return ag_object_str(ctx);
+}
+
+
+extern bool ag_url_secure(const ag_url_t *ctx);
+extern ag_string_t *ag_url_host(const ag_url_t *ctx);
+extern ag_string_t *ag_url_port(const ag_url_t *ctx);
+extern ag_string_t *ag_url_path(const ag_url_t *ctx);
+
 
 
 extern const char *ag_http_mime_str(enum ag_http_mime type);
