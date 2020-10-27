5a52649984c8941611a5bff8aad6894c4293ccb5
Abhishek Chakravarti <abhishek@taranjali.org>
2020-10-27 21:08:20 +0530

    Declare interface for ag_response_t
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/api.h
+++ src/api.h
@@ -1062,3 +1062,82 @@ extern void ag_http_response_file(const char *fpath);
 
 extern ag_url_t *ag_http_request_url(void);
 
+
+typedef ag_object_t ag_response_t;
+#define ag_response_smart_t ag_object_smart_t
+extern void ag_response_register(void);
+
+extern ag_response_t *ag_response_new(enum ag_http_mime type, 
+        enum ag_http_status code);
+
+inline ag_response_t *ag_response_copy(const ag_response_t *ctx)
+{
+    return ag_object_copy(ctx);
+}
+
+
+inline void ag_response_dispose(ag_response_t **ctx)
+{
+    ag_object_dispose(ctx);
+}
+
+inline size_t ag_response_typeid(const ag_response_t *ctx)
+{
+    return ag_object_type(ctx);
+}
+
+inline size_t ag_response_objid(const ag_response_t *ctx)
+{
+    return ag_object_id(ctx);
+}
+
+inline size_t ag_response_hash(const ag_response_t *ctx)
+{
+    return ag_object_hash(ctx);
+}
+
+inline size_t ag_response_sz(const ag_response_t *ctx)
+{
+    return ag_object_sz(ctx);
+}
+
+inline size_t ag_response_len(const ag_response_t *ctx)
+{
+    return ag_object_len(ctx);
+}
+
+inline bool ag_response_empty(const ag_response_t *ctx)
+{
+    return ag_object_empty(ctx);
+}
+
+inline enum ag_tristate ag_response_cmp(const ag_response_t *ctx, 
+        const ag_response_t *cmp)
+{
+    return ag_object_cmp(ctx, cmp);
+}
+
+inline bool ag_response_lt(const ag_response_t *ctx, const ag_response_t *cmp)
+{
+    return ag_object_lt(ctx, cmp);
+}
+
+inline bool ag_response_eq(const ag_response_t *ctx, const ag_response_t *cmp)
+{
+    return ag_object_eq(ctx, cmp);
+}
+
+inline bool ag_response_gt(const ag_response_t *ctx, const ag_response_t *cmp)
+{
+    return ag_object_gt(ctx, cmp);
+}
+
+inline ag_string_t *ag_response_str(const ag_response_t *ctx)
+{
+    return ag_object_str(ctx);
+}
+
+
+extern void ag_response_add(const ag_response_t *ctx);
+extern void ag_response_add_file(const char *fpath);
+
