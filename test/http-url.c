2d480c6d3959a58ffcca90433332386921893501
Abhishek Chakravarti <abhishek@taranjali.org>
2020-11-18 12:50:48 +0530

    Add manager test cases for ag_http_url_t
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/http-url.c
+++ test/http-url.c
@@ -11,36 +11,76 @@
 
 static void new_01(void)
 {
-    printf("ag_http_url_new(): @secure true, host && path && port ~empty"
+    printf("ag_http_url_new(): @secure true, @host && @path && @port ~empty"
            " => new url");
+
+    ag_http_url_smart_t *u = ag_http_url_new(true, "example.com", "80", "foo");
+    ag_require (u, AG_ERNO_TEST, NULL);
+    
+    ag_require (ag_http_url_secure(u), AG_ERNO_TEST, NULL);
+
+    ag_string_smart_t *h = ag_http_url_host(u);
+    ag_require (ag_string_eq(h, "example.com"), AG_ERNO_TEST, NULL);
+
+    ag_string_smart_t *p = ag_http_url_path(u);
+    ag_require (ag_string_eq(p, "foo"), AG_ERNO_TEST, NULL);
+
+    ag_string_smart_t *port = ag_http_url_port(u);
+    ag_require (ag_string_eq(port, "80"), AG_ERNO_TEST, NULL);
+
     printf("...OK\n");
 }
 
 static void copy_01(void)
 {
-    printf("ag_http_url_new(): @secure true, host && path && port ~empty"
-           " => new url");
+    printf("ag_http_url_copy(): @ctx ~null => @ctx");
+    
+    ag_http_url_smart_t *u1 = ag_http_url_new(false, "example.com", "80", "foo");
+    ag_http_url_smart_t *u2 = ag_http_url_copy(u1);
+    ag_require (u2, AG_ERNO_TEST, NULL);
+    ag_require (!ag_http_url_secure(u1), AG_ERNO_TEST, NULL);
+
+    ag_string_smart_t *host = ag_http_url_host(u2);
+    ag_require (ag_string_eq(host, "example.com"), AG_ERNO_TEST, NULL);
+
+    ag_string_smart_t *path = ag_http_url_path(u2);
+    ag_require (ag_string_eq(path, "foo"), AG_ERNO_TEST, NULL);
+
+    ag_string_smart_t *port = ag_http_url_port(u2);
+    ag_require (ag_string_eq(port, "80"), AG_ERNO_TEST, NULL);
+
     printf("...OK\n");
 }
 
 static void dispose_01(void)
 {
-    printf("ag_http_url_dispose(): @secure true, host && path && port ~empty"
-           " => new url");
+    printf("ag_http_url_dispose(): @ctx valid => @ctx null");
+
+    ag_http_url_smart_t *u = ag_http_url_new(false, "example.com", "path",
+            "80");
+    ag_http_url_dispose(&u);
+    ag_require (!u, AG_ERNO_TEST, NULL);
+
     printf("...OK\n");
 }
 
 static void dispose_02(void)
 {
-    printf("ag_http_url_dispose(): @secure true, host && path && port ~empty"
-           " => new url");
+    printf("ag_http_url_dispose(): @ctx null => no-op");
+
+    ag_http_url_dispose(NULL);
+
     printf("...OK\n");
 }
 
 static void dispose_03(void)
 {
-    printf("ag_http_url_dispose(): @secure true, host && path && port ~empty"
-           " => new url");
+    printf("ag_http_url_dispose(): @ctx ~null && ptrval null => @ctx null");
+
+    ag_http_url_t *u = NULL;
+    ag_http_url_dispose(&u);
+    ag_require (!u, AG_ERNO_TEST, NULL);
+
     printf("...OK\n");
 }
 
