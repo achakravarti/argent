cfe6b83682b78974a1ad0b80e7e0bc9ff002a835
Abhishek Chakravarti <abhishek@taranjali.org>
2020-11-20 13:01:40 +0530

    Add comparator test cases for ag_http_url_t
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/http-url.c
+++ test/http-url.c
@@ -9,6 +9,9 @@
  */
 
 
+/*
+ * new_01(): ag_http_url_new() test #1.
+ */
 static void new_01(void)
 {
     printf("ag_http_url_new(): @secure true, @host && @path ~empty, @port = 80"
@@ -30,6 +33,9 @@ static void new_01(void)
 }
 
 
+/*
+ * new_02(): ag_http_url_new() test #2.
+ */
 static void new_02(void)
 {
     printf("ag_http_url_new(): @secure false, @host && @path ~empty, @port = 0"
@@ -51,6 +57,9 @@ static void new_02(void)
 }
 
 
+/*
+ * new_03(): ag_http_url_new() test #3.
+ */
 static void new_03(void)
 {
     printf("ag_http_url_new(): @secure true, @host ~empty, @path empty,"
@@ -72,6 +81,9 @@ static void new_03(void)
 }
 
 
+/*
+ * copy_01(): ag_http_url_copy() test #1.
+ */
 static void copy_01(void)
 {
     printf("ag_http_url_copy(): @ctx ~null => @ctx");
@@ -92,6 +104,10 @@ static void copy_01(void)
     printf("...OK\n");
 }
 
+
+/*
+ * dispose_01(): ag_http_url_dispose() test #1.
+ */
 static void dispose_01(void)
 {
     printf("ag_http_url_dispose(): @ctx valid => @ctx null");
@@ -103,6 +119,10 @@ static void dispose_01(void)
     printf("...OK\n");
 }
 
+
+/*
+ * dispose_02(): ag_http_url_dispose() test #2.
+ */
 static void dispose_02(void)
 {
     printf("ag_http_url_dispose(): @ctx null => no-op");
@@ -112,6 +132,10 @@ static void dispose_02(void)
     printf("...OK\n");
 }
 
+
+/*
+ * dispose_03(): ag_http_url_dispose() test #3.
+ */
 static void dispose_03(void)
 {
     printf("ag_http_url_dispose(): @ctx ~null && ptrval null => @ctx null");
@@ -122,6 +146,194 @@ static void dispose_03(void)
 
     printf("...OK\n");
 }
+
+
+
+
+/*******************************************************************************
+ *                               COMPARATOR TESTS
+ */
+
+
+/*
+ * cmp_01(): ag_http_url_cmp() test #1.
+ */
+static void cmp_01(void)
+{
+    printf("ag_http_url_cmp(): @ctx < @cmp => AG_TRISTATE_LO");
+
+    ag_http_url_smart_t *u1 = ag_http_url_new(false, "example.com", 0, "");
+    ag_http_url_smart_t *u2 = ag_http_url_new(true, "abc.net", 0, "");
+    ag_require (ag_http_url_cmp(u1, u2) == AG_TRISTATE_LO, AG_ERNO_TEST, NULL);
+
+    printf("...OK\n");
+}
+
+
+/*
+ * cmp_02(): ag_http_url_cmp() test #2.
+ */
+static void cmp_02(void)
+{
+    printf("ag_http_url_cmp(): @ctx == @cmp => AG_TRISTATE_GND");
+
+    ag_http_url_smart_t *u1 = ag_http_url_new(true, "example.com", 8080, "foo");
+    ag_http_url_smart_t *u2 = ag_http_url_copy(u1);
+    ag_require (ag_http_url_cmp(u1, u2) == AG_TRISTATE_GND, AG_ERNO_TEST, NULL);
+
+    printf("...OK\n");
+}
+
+
+/*
+ * cmp_03(): ag_http_url_cmp() test #3.
+ */
+static void cmp_03(void)
+{
+    printf("ag_http_url_cmp(): @ctx > @cmp => AG_TRISTATE_HI");
+
+    ag_http_url_smart_t *u1 = ag_http_url_new(false, "example.com", 0, "");
+    ag_http_url_smart_t *u2 = ag_http_url_new(true, "abc.net", 0, "");
+    ag_require (ag_http_url_cmp(u2, u1) == AG_TRISTATE_HI, AG_ERNO_TEST, NULL);
+
+    printf("...OK\n");
+}
+
+
+/*
+ * lt_01(): ag_http_url_lt() test #1.
+ */
+static void lt_01(void)
+{
+    printf("ag_http_url_lt(): @ctx < @cmp => true");
+
+    ag_http_url_smart_t *u1 = ag_http_url_new(false, "example.com", 0, "");
+    ag_http_url_smart_t *u2 = ag_http_url_new(true, "abc.net", 0, "");
+    ag_require (ag_http_url_lt(u1, u2), AG_ERNO_TEST, NULL);
+
+    printf("...OK\n");
+}
+
+
+/*
+ * lt_02(): ag_http_url_lt() test #2.
+ */
+static void lt_02(void)
+{
+    printf("ag_http_url_lt(): @ctx == @cmp => false");
+    
+    ag_http_url_smart_t *u1 = ag_http_url_new(true, "example.com", 8080, "foo");
+    ag_http_url_smart_t *u2 = ag_http_url_copy(u1);
+    ag_require (!ag_http_url_lt(u1, u2), AG_ERNO_TEST, NULL);
+
+    printf("...OK\n");
+}
+
+
+/*
+ * lt_03(): ag_http_url_lt() test #3.
+ */
+static void lt_03(void)
+{
+    printf("ag_http_url_lt(): @ctx > @cmp => false");
+    
+    ag_http_url_smart_t *u1 = ag_http_url_new(false, "example.com", 0, "");
+    ag_http_url_smart_t *u2 = ag_http_url_new(true, "abc.net", 0, "");
+    ag_require (!ag_http_url_lt(u2, u1), AG_ERNO_TEST, NULL);
+   
+    printf("...OK\n");
+}
+
+
+/*
+ * eq_01(): ag_http_url_eq() test #1.
+ */
+static void eq_01(void)
+{
+    printf("ag_http_url_eq(): @ctx == @cmp => true");
+    
+    ag_http_url_smart_t *u1 = ag_http_url_new(true, "example.com", 8080, "foo");
+    ag_http_url_smart_t *u2 = ag_http_url_copy(u1);
+    ag_require (ag_http_url_eq(u1, u2), AG_ERNO_TEST, NULL);
+   
+    printf("...OK\n");
+}
+
+
+/*
+ * eq_02(): ag_http_url_eq() test #2.
+ */
+static void eq_02(void)
+{
+    printf("ag_http_url_eq(): @ctx < @cmp => false");
+    
+    ag_http_url_smart_t *u1 = ag_http_url_new(false, "example.com", 0, "");
+    ag_http_url_smart_t *u2 = ag_http_url_new(true, "abc.net", 0, "");
+    ag_require (!ag_http_url_eq(u1, u2), AG_ERNO_TEST, NULL);
+
+    printf("...OK\n");
+}
+
+
+/*
+ * eq_03(): ag_http_url_eq() test #3.
+ */
+static void eq_03(void)
+{
+    printf("ag_http_url_eq(): @ctx > @cmp => false");
+
+    ag_http_url_smart_t *u1 = ag_http_url_new(false, "example.com", 0, "");
+    ag_http_url_smart_t *u2 = ag_http_url_new(true, "abc.net", 0, "");
+    ag_require (!ag_http_url_eq(u2, u1), AG_ERNO_TEST, NULL);
+
+    printf("...OK\n");
+}
+
+
+/*
+ * gt_01(): ag_http_url_gt() test #1.
+ */
+static void gt_01(void)
+{
+    printf("ag_http_url_gt(): @ctx > @cmp => true");
+    
+    ag_http_url_smart_t *u1 = ag_http_url_new(false, "example.com", 0, "");
+    ag_http_url_smart_t *u2 = ag_http_url_new(true, "abc.net", 0, "");
+    ag_require (ag_http_url_gt(u2, u1), AG_ERNO_TEST, NULL);
+   
+    printf("...OK\n");
+}
+
+
+/*
+ * gt_02(): ag_http_url_gt() test #2.
+ */
+static void gt_02(void)
+{
+    printf("ag_http_url_gt(): @ctx == @cmp => false");
+    
+    ag_http_url_smart_t *u1 = ag_http_url_new(true, "example.com", 8080, "foo");
+    ag_http_url_smart_t *u2 = ag_http_url_copy(u1);
+    ag_require (!ag_http_url_gt(u1, u2), AG_ERNO_TEST, NULL);
+
+    printf("...OK\n");
+}
+
+
+/*
+ * gt_03(): ag_http_url_gt() test #3.
+ */
+static void gt_03(void)
+{
+    printf("ag_http_url_gt(): @ctx < @cmp => false");
+    
+    ag_http_url_smart_t *u1 = ag_http_url_new(false, "example.com", 0, "");
+    ag_http_url_smart_t *u2 = ag_http_url_new(true, "abc.net", 0, "");
+    ag_require (!ag_http_url_gt(u1, u2), AG_ERNO_TEST, NULL);
+
+    printf("...OK\n");
+}
+
 
 
 
@@ -144,6 +356,19 @@ extern void ag_test_http_url(void)
     dispose_01();
     dispose_02();
     dispose_03();
+
+    cmp_01();
+    cmp_02();
+    cmp_03();
+    lt_01();
+    lt_02();
+    lt_03();
+    eq_01();
+    eq_02();
+    eq_03();
+    gt_01();
+    gt_02();
+    gt_03();
     
     printf("\n");
 }
