9bc1fab1c952b68243a5ac6232b672146d5a2867
Abhishek Chakravarti <abhishek@taranjali.org>
2020-11-27 13:24:03 +0530

    Add comparator test cases for ag_ip_t
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/ip.c
+++ test/ip.c
@@ -10,6 +10,12 @@ static inline ag_ip_t *sample_localhost(void)
 }
 
 
+static inline ag_ip_t *sample_private(void)
+{
+    return ag_ip_parse("192.168.1.4");
+}
+
+
 
 
 /*******************************************************************************
@@ -120,6 +126,193 @@ static void dispose_03(void)
 
 
 /*******************************************************************************
+ *                               COMPARATOR TESTS
+ */
+
+
+/*
+ * cmp_01(): ag_ip_cmp() test #1.
+ */
+static void cmp_01(void)
+{
+    printf("ag_ip_cmp(): @ctx < @cmp => AG_TRISTATE_LO");
+
+    ag_ip_smart_t *ip1 = sample_localhost();
+    ag_ip_smart_t *ip2 = sample_private();
+    ag_test (ag_ip_cmp(ip1, ip2) == AG_TRISTATE_LO);
+
+    printf("...OK\n");
+}
+
+
+/*
+ * cmp_02(): ag_ip_cmp() test #2.
+ */
+static void cmp_02(void)
+{
+    printf("ag_ip_cmp(): @ctx == @cmp => AG_TRISTATE_GND");
+
+    ag_ip_smart_t *ip1 = sample_localhost();
+    ag_ip_smart_t *ip2 = ag_ip_copy(ip1);
+    ag_test (ag_ip_cmp(ip1, ip2) == AG_TRISTATE_GND);
+
+    printf("...OK\n");
+}
+
+
+/*
+ * cmp_03(): ag_ip_cmp() test #3.
+ */
+static void cmp_03(void)
+{
+    printf("ag_ip_cmp(): @ctx > @cmp => AG_TRISTATE_HI");
+
+    ag_ip_smart_t *ip1 = sample_localhost();
+    ag_ip_smart_t *ip2 = sample_private();
+    ag_test (ag_ip_cmp(ip2, ip1) == AG_TRISTATE_HI);
+
+    printf("...OK\n");
+}
+
+
+/*
+ * lt_01(): ag_ip_lt() test #1.
+ */
+static void lt_01(void)
+{
+    printf("ag_ip_lt(): @ctx < @cmp => true");
+
+    ag_ip_smart_t *ip1 = sample_localhost();
+    ag_ip_smart_t *ip2 = sample_private();
+    ag_test (ag_ip_lt(ip1, ip2));
+
+    printf("...OK\n");
+}
+
+
+/*
+ * lt_02(): ag_ip_lt() test #2.
+ */
+static void lt_02(void)
+{
+    printf("ag_ip_lt(): @ctx == @cmp => false");
+
+    ag_ip_smart_t *ip1 = sample_localhost();
+    ag_ip_smart_t *ip2 = ag_ip_copy(ip1);
+    ag_test (!ag_ip_lt(ip1, ip2));
+
+    printf("...OK\n");
+}
+
+
+/*
+ * lt_03(): ag_ip_lt() test #3.
+ */
+static void lt_03(void)
+{
+    printf("ag_ip_lt(): @ctx > @cmp => false");
+
+    ag_ip_smart_t *ip1 = sample_localhost();
+    ag_ip_smart_t *ip2 = sample_private();
+    ag_test (!ag_ip_lt(ip2, ip1));
+
+    printf("...OK\n");
+}
+
+
+/*
+ * eq_01(): ag_ip_eq() test #1.
+ */
+static void eq_01(void)
+{
+    printf("ag_ip_eq(): @ctx == @cmp => true");
+
+    ag_ip_smart_t *ip1 = sample_localhost();
+    ag_ip_smart_t *ip2 = ag_ip_copy(ip1);
+    ag_test (ag_ip_eq(ip1, ip2));
+
+    printf("...OK\n");
+}
+
+
+/*
+ * eq_02(): ag_ip_eq() test #2.
+ */
+static void eq_02(void)
+{
+    printf("ag_ip_eq(): @ctx < @cmp => false");
+
+    ag_ip_smart_t *ip1 = sample_localhost();
+    ag_ip_smart_t *ip2 = sample_private();
+    ag_test (!ag_ip_eq(ip1, ip2));
+
+    printf("...OK\n");
+}
+
+
+/*
+ * eq_03(): ag_ip_eq() test #3.
+ */
+static void eq_03(void)
+{
+    printf("ag_ip_eq(): @ctx > @cmp => false");
+
+    ag_ip_smart_t *ip1 = sample_localhost();
+    ag_ip_smart_t *ip2 = sample_private();
+    ag_test (!ag_ip_eq(ip2, ip1));
+
+    printf("...OK\n");
+}
+
+
+/*
+ * gt_01(): ag_ip_gt() test #1.
+ */
+static void gt_01(void)
+{
+    printf("ag_ip_gt(): @ctx > @cmp => true");
+
+    ag_ip_smart_t *ip1 = sample_localhost();
+    ag_ip_smart_t *ip2 = sample_private();
+    ag_test (ag_ip_gt(ip2, ip1));
+
+    printf("...OK\n");
+}
+
+
+/*
+ * gt_02(): ag_ip_gt() test #2.
+ */
+static void gt_02(void)
+{
+    printf("ag_ip_gt(): @ctx == @cmp => false");
+
+    ag_ip_smart_t *ip1 = sample_localhost();
+    ag_ip_smart_t *ip2 = ag_ip_copy(ip1);
+    ag_test (!ag_ip_gt(ip1, ip2));
+
+    printf("...OK\n");
+}
+
+
+/*
+ * gt_03(): ag_ip_gt() test #3.
+ */
+static void gt_03(void)
+{
+    printf("ag_ip_gt(): @ctx < @cmp => false");
+
+    ag_ip_smart_t *ip1 = sample_localhost();
+    ag_ip_smart_t *ip2 = sample_private();
+    ag_test (!ag_ip_gt(ip1, ip2));
+
+    printf("...OK\n");
+}
+
+
+
+
+/*******************************************************************************
  *                             TEST SUITE INTERFACE
  */
 
@@ -137,5 +330,18 @@ extern void ag_test_ip(void)
     dispose_01();
     dispose_02();
     dispose_03();
+
+    cmp_01();
+    cmp_02();
+    cmp_03();
+    lt_01();
+    lt_02();
+    lt_03();
+    eq_01();
+    eq_02();
+    eq_03();
+    gt_01();
+    gt_02();
+    gt_03();
 }
 
