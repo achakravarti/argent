62852a6fbceb346f35446967a95579520dec5257
Abhishek Chakravarti <abhishek@taranjali.org>
2020-12-07 00:58:07 +0530

    Use ag_test_init() and ag_test_exit() macros in tests
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/mblock.c
+++ test/mblock.c
@@ -7,147 +7,140 @@ struct test {
 };
 
 
-#define NEW_01 "ag_mblock_new() allocates memory on the heap for an int"
-static enum ag_test_status new_01(void)
+ag_test_init(new_01, "ag_mblock_new() allocates memory on the heap for an int")
 {
         int *i = ag_mblock_new(sizeof *i);
-        bool ok = i;
-        
+        ag_test_assert (i);
         ag_mblock_free((ag_mblock **)&i);
-        return ag_test_assert (ok);
 }
+ag_test_exit()
 
 
-#define NEW_02 "ag_mblock_new() allocates memory on the heap for a test struct"
-static enum ag_test_status new_02(void)
+ag_test_init(new_02, "ag_mblock_new() allocates memory on the heap for a"
+                " tests truct")
 {
         struct test *t = ag_mblock_new(sizeof *t);
         t->i = ag_mblock_new(sizeof *t->i);
         t->j = ag_mblock_new(sizeof *t->j);
 
-        bool ok = t && t->i && t->j;
+        ag_test_assert (t && t->i && t->j);
         
         ag_mblock_free((ag_mblock **)&t->i);
         ag_mblock_free((ag_mblock **)&t->j);
         ag_mblock_free((ag_mblock **)&t);
 
-        return ag_test_assert (ok);
 }
+ag_test_exit()
 
 
-#define NEW_03 "ag_mblock_new() returns a block with a reference count of 1"
-static enum ag_test_status new_03(void)
+ag_test_init(new_03, "ag_mblock_new() returns a block with a reference count"
+                " of 1")
 {
         int *i = ag_mblock_new(sizeof *i);
-        bool ok = ag_mblock_refc((ag_mblock *)i) == 1;
-        
+        ag_test_assert (ag_mblock_refc(i) == 1);
         ag_mblock_free((ag_mblock **)&i);
-        return ag_test_assert (ok);
 }
+ag_test_exit()
 
 
-#define NEW_04 "ag_mblock_new() returns a block with the requested data size"
-static enum ag_test_status new_04(void)
+ag_test_init(new_04, "ag_mblock_new() returns a block with the requested data"
+                " size")
 {
         int *i = ag_mblock_new(sizeof *i);
-        bool ok = ag_mblock_sz((ag_mblock *)i) == sizeof(int);
-        
+        ag_test_assert (ag_mblock_sz(i) == sizeof(int));
         ag_mblock_free((ag_mblock **)&i);
-        return ag_test_assert (ok);
 }
+ag_test_exit()
 
 
-#define NEW_05 "ag_mblock_new() returns a block with a total size >=" \
-                " requested data size"
-static enum ag_test_status new_05(void)
+ag_test_init(new_05, "ag_mblock_new() returns a block with a total size >="
+                " requested data size")
 {
         int *i = ag_mblock_new(sizeof *i);
-        bool ok = ag_mblock_sz_total((ag_mblock *)i) >= sizeof(int);
-        
+        ag_test_assert (ag_mblock_sz_total(i) >= sizeof(int));
         ag_mblock_free((ag_mblock **)&i);
-        return ag_test_assert (ok);
 }
+ag_test_exit()
 
 
-#define NEW_ALIGN_01 "ag_mblock_new_align() allocates memory on the heap for" \
-                " an int"
-static enum ag_test_status new_align_01(void)
+ag_test_init(new_align_01, "ag_mblock_new_align() allocates memory on the heap"
+                " for an int")
 {
         int *i = ag_mblock_new_align(sizeof *i, 8);
-        bool ok = i;
-        
+        ag_test_assert (i);
         ag_mblock_free((ag_mblock **)&i);
-        return ag_test_assert (ok);
 }
+ag_test_exit()
 
 
-#define NEW_ALIGN_02 "ag_mblock_new_align() allocates memory on the heap" \
-                " for a test struct"
-static enum ag_test_status new_align_02(void)
+ag_test_init(new_align_02, "ag_mblock_new_align() allocates memory on the heap"
+                " for a test struct")
 {
         struct test *t = ag_mblock_new_align(sizeof *t, 8);
         t->i = ag_mblock_new_align(sizeof *t->i, 32);
         t->j = ag_mblock_new_align(sizeof *t->j, 32);
 
-        bool ok = t && t->i && t->j;
+        ag_test_assert (t && t->i && t->j);
         
         ag_mblock_free((ag_mblock **)&t->i);
         ag_mblock_free((ag_mblock **)&t->j);
         ag_mblock_free((ag_mblock **)&t);
-
-        return ag_test_assert (ok);
 }
+ag_test_exit()
 
 
-#define NEW_ALIGN_03 "ag_mblock_new_align() returns a block with a reference" \
-                " count of 1"
-static enum ag_test_status new_align_03(void)
+ag_test_init(new_align_03, "ag_mblock_new_align() returns a block with a"
+                "  reference count of 1")
 {
         int *i = ag_mblock_new_align(sizeof *i, 8);
-        bool ok = ag_mblock_refc((ag_mblock *)i) == 1;
-        
+        ag_test_assert (ag_mblock_refc(i) == 1);
         ag_mblock_free((ag_mblock **)&i);
-        return ag_test_assert (ok);
 }
+ag_test_exit()
 
 
-#define NEW_ALIGN_04 "ag_mblock_new_align() returns a block with the" \
-                " requested data size"
-static enum ag_test_status new_align_04(void)
+ag_test_init(new_align_04, "ag_mblock_new_align() returns a block with the"
+                " requested data size")
 {
         int *i = ag_mblock_new_align(sizeof *i, 8);
-        bool ok = ag_mblock_sz((ag_mblock *)i) == sizeof(int);
-        
+        ag_test_assert (ag_mblock_sz(i) == sizeof(int));
         ag_mblock_free((ag_mblock **)&i);
-        return ag_test_assert (ok);
 }
+ag_test_exit()
 
 
-#define NEW_ALIGN_05 "ag_mblock_new_align() returns a block with a total" \
-                " size >= requested data size"
-static enum ag_test_status new_align_05(void)
+ag_test_init(new_align_05, "ag_mblock_new_align() returns a block with a total"
+                " size >= requested data size")
 {
         int *i = ag_mblock_new_align(sizeof *i, 8);
-        bool ok = ag_mblock_sz_total((ag_mblock *)i) >= sizeof(int);
-        
+        ag_test_assert (ag_mblock_sz_total(i) >= sizeof(int));
         ag_mblock_free((ag_mblock **)&i);
-        return ag_test_assert (ok);
 }
+ag_test_exit()
 
 
-#define NEW_ALIGN_06 "ag_mblock_new_align() returns a block with the required" \
-                " alignment"
-static enum ag_test_status new_align_06(void)
+ag_test_init(new_align_06, "ag_mblock_new_align() returns a block with the"
+                " required alignment")
 {
         int *i = ag_mblock_new_align(sizeof *i, 32);
-        bool ok = ag_mblock_aligned((ag_mblock *)i, 32);
-        
+        ag_test_assert (ag_mblock_aligned(i, 32));
         ag_mblock_free((ag_mblock **)&i);
-        return ag_test_assert (ok);
 }
+ag_test_exit()
 
 
+ag_test_init(copy_01, "ag_mblock_copy() makes a copy of an int in the heap")
+{
+        int *i = ag_mblock_new(sizeof *i);
+        *i = 555;
 
+        int *j = ag_mblock_copy(i);
+        ag_test_assert ((*j == 555));
+
+        ag_mblock_free((ag_mblock **)&i);
+        ag_mblock_free((ag_mblock **)&j);
+}
+ag_test_exit()
 
 
 extern ag_test_suite *ag_test_suite_mblock(void)
@@ -164,20 +157,22 @@ extern ag_test_suite *ag_test_suite_mblock(void)
                 &new_align_04,
                 &new_align_05,
                 &new_align_06,
+                &copy_01,
         };
 
         const char *desc[] = {
-                NEW_01,
-                NEW_02,
-                NEW_03,
-                NEW_04,
-                NEW_05,
-                NEW_ALIGN_01,
-                NEW_ALIGN_02,
-                NEW_ALIGN_03,
-                NEW_ALIGN_04,
-                NEW_ALIGN_05,
-                NEW_ALIGN_06,
+                new_01_desc,
+                new_02_desc,
+                new_03_desc,
+                new_04_desc,
+                new_05_desc,
+                new_align_01_desc,
+                new_align_02_desc,
+                new_align_03_desc,
+                new_align_04_desc,
+                new_align_05_desc,
+                new_align_06_desc,
+                copy_01_desc,
         };
 
         ag_test_suite *ctx = ag_test_suite_new("ag_mblock interface");
