011db6fb922f84ced659db05e5954bb1f79446d1
Abhishek Chakravarti <abhishek@taranjali.org>
2020-12-17 20:45:30 +0530

    Declare new string interface
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ include/str.h
@@ -0,0 +1,94 @@
+/*-
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * Argent - infrastructure for building web services
+ * Copyright (C) 2020 Abhishek Chakravarti
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * You can contact Abhishek Chakravarti at <abhishek@taranjali.org>.
+ */
+
+
+#ifndef __ARGENT_STR_H__
+#define __ARGENT_STR_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include "./argent.h"
+
+
+typedef char ag_str;
+
+#if (defined __GNUC__ || defined __clang__)
+#       define ag_str_auto __attribute__((cleanup(ag_str_release))) ag_str
+#else
+#       define ag_str_auto ag_str
+#       warning "[!] ag_str_auto leaks memory on current compiler"
+#endif
+
+
+extern ag_str *ag_str_new(const char *cstr);
+
+inline ag_str *ag_str_new_empty(void)
+{
+    return ag_str_new("");
+}
+
+extern ag_str *ag_str_new_fmt(const char *fmt, ...);
+extern ag_str *ag_str_copy(const ag_str *ctx);
+extern void ag_str_dispose(ag_str **ctx);
+
+
+extern enum ag_cmp ag_str_cmp(const char *,  const char *);
+
+inline bool ag_str_lt(const char *ctx, const char *cmp)
+{
+    return ag_str_cmp(ctx, cmp) == AG_CMP_LT;
+}
+
+inline bool ag_str_eq(const char *ctx, const char *cmp)
+{
+    return ag_str_cmp(ctx, cmp) == AG_CMP_EQ;
+}
+
+inline bool ag_str_gt(const char *ctx, const char *cmp)
+{
+    return ag_str_cmp(ctx, cmp) == AG_CMP_GT;
+}
+
+
+extern bool ag_str_empty(const ag_str *);
+extern bool ag_str_has(const ag_str *, const char *);
+extern size_t ag_str_len(const ag_str *);
+extern size_t ag_str_sz(const ag_str *);
+extern size_t ag_str_refc(const ag_str *);
+
+
+extern ag_str *ag_str_lower(const char *);
+extern ag_str *ag_str_upper(const char *);
+extern ag_str *ag_str_proper(const char *);
+extern ag_str *ag_str_split(const char *, const char *);
+extern ag_str *ag_str_split_right(const char *, const char *);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !__ARGENT_STR_H__ */
+
