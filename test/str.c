7ffa76968988d4cfa3166a18a53606efa1eb5c8f
Abhishek Chakravarti <abhishek@taranjali.org>
2020-12-18 19:31:54 +0530

    Add test cases for ag_str_cmp()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/str.c
+++ test/str.c
@@ -94,13 +94,19 @@ AG_TEST_INIT(copy_04, "ag_str_copy() increases the reference count by 1") {
 } AG_TEST_EXIT();
 
 
+/*
+ * The following unit tests check whether ag_str_release() releases a string
+ * correctly, or perform a safe no-op if provided invalid strings.
+ */
+
+
 AG_TEST_INIT(release_01, "ag_str_release() performs a no-op if passed NULL") {
         ag_str_release(NULL);
         AG_TEST_ASSERT (true);
 } AG_TEST_EXIT();
 
 
-AG_TEST_INIT(release_02, "ag_str_release() perfroms a no-op if passed a handle"
+AG_TEST_INIT(release_02, "ag_str_release() performs a no-op if passed a handle"
                 " to NULL") {
         ag_str *s = NULL;
         ag_str_release(&s);
@@ -125,20 +131,93 @@ AG_TEST_INIT(release_04, "ag_str_release() reduces the reference count by 1") {
 } AG_TEST_EXIT();
 
 
+AG_TEST_INIT(cmp_01, "ag_str_cmp() returns AG_CMP_EQ when comparing two empty"
+                " strings") {
+        ag_str_auto *s = ag_str_new_empty();
+        ag_str_auto *s2 = ag_str_new_empty();
+        AG_TEST_ASSERT (ag_str_cmp(s, s2) == AG_CMP_EQ);
+} AG_TEST_EXIT();
+
+
+AG_TEST_INIT(cmp_02, "ag_str_cmp() return AG_CMP_LT when comparing an empty"
+                " string with another string") {
+        ag_str_auto *s = ag_str_new_empty();
+        ag_str_auto *s2 = ag_str_new("Hello, world!");
+        AG_TEST_ASSERT (ag_str_cmp(s, s2) == AG_CMP_LT);
+} AG_TEST_EXIT();
+
+
+AG_TEST_INIT(cmp_03, "ag_str_cmp() return AG_CMP_EQ when comparing two equal"
+                " ASCII strings") {
+        ag_str_auto *s = ag_str_new("Hello, world!");
+        ag_str_auto *s2 = ag_str_copy(s);
+        AG_TEST_ASSERT (ag_str_cmp(s, s2) == AG_CMP_EQ);
+} AG_TEST_EXIT();
+
+
+AG_TEST_INIT(cmp_04, "ag_str_cmp() returns AG_CMP_LT when comparing an ASCII"
+                " string that is lexicographically less than another") {
+        ag_str_auto *s = ag_str_new("Goodbye, world!");
+        ag_str_auto *s2 = ag_str_new("Hello, world!");
+        AG_TEST_ASSERT (ag_str_cmp(s, s2) == AG_CMP_LT);
+} AG_TEST_EXIT();
+
+
+AG_TEST_INIT(cmp_05, "ag_str_cmp() returns AG_CMP_GT when comparing an ASCII"
+                " string that is lexicographically greater than another") {
+        ag_str_auto *s = ag_str_new("Goodbye, world!");
+        ag_str_auto *s2 = ag_str_new("Hello, world!");
+        AG_TEST_ASSERT (ag_str_cmp(s2, s) == AG_CMP_GT);
+} AG_TEST_EXIT();
+
+AG_TEST_INIT(cmp_06, "ag_str_cmp() returns AG_CMP_GT when comparing an ASCII"
+                " string to an empty string") {
+        ag_str_auto *s = ag_str_new_empty();
+        ag_str_auto *s2 = ag_str_new("Hello, world!");
+        AG_TEST_ASSERT (ag_str_cmp(s2, s) == AG_CMP_GT);
+} AG_TEST_EXIT();
+
+
+AG_TEST_INIT(cmp_07, "ag_str_cmp() returns AG_CMP_EQ when comparing two equal"
+                " Unicode strings") {
+        ag_str_auto *s = ag_str_new("नमस्ते दुनिया!");
+        ag_str_auto *s2 = ag_str_copy(s);
+        AG_TEST_ASSERT (ag_str_cmp(s, s2) == AG_CMP_EQ);
+} AG_TEST_EXIT();
+
+
+AG_TEST_INIT(cmp_08, "ag_str_cmp() returns AG_CMP_LT when comparing a Unicode"
+                " string that is lexicographically less than another") {
+        ag_str_auto *s = ag_str_new("नमस्ते दुनिया!");
+        ag_str_auto *s2 = ag_str_new("अभिषेक बहुत बुरा गाता है।");
+        AG_TEST_ASSERT (ag_str_cmp(s2, s) == AG_CMP_LT);
+} AG_TEST_EXIT();
+
+
+AG_TEST_INIT(cmp_09, "ag_str_cmp() return AG_CMP_GT when comparing a Unicode"
+                " string that is lexicographically greater than another") {
+        ag_str_auto *s = ag_str_new("नमस्ते दुनिया!");
+        ag_str_auto *s2 = ag_str_new("अभिषेक बहुत बुरा गाता है।");
+        AG_TEST_ASSERT (ag_str_cmp(s, s2) == AG_CMP_GT);
+} AG_TEST_EXIT();
+
 
 extern ag_test_suite *test_suite_str(void)
 {
         ag_test *test[] = {
                 new_01, new_02, new_03, new_empty_01, new_fmt_01, new_fmt_02,
                 new_fmt_03, copy_01, copy_02, copy_03, copy_04, release_01,
-                release_02, release_03, release_04,
+                release_02, release_03, release_04, cmp_01, cmp_02, cmp_03,
+                cmp_04, cmp_05, cmp_06, cmp_07, cmp_08, cmp_09,
         };
 
         const char *desc[] = {
                 new_01_desc, new_02_desc, new_03_desc, new_empty_01_desc,
                 new_fmt_01_desc, new_fmt_02_desc, new_fmt_03_desc, copy_01_desc,
                 copy_02_desc, copy_03_desc, copy_04_desc, release_01_desc,
-                release_02_desc, release_03_desc, release_04_desc,
+                release_02_desc, release_03_desc, release_04_desc, cmp_01_desc,
+                cmp_02_desc, cmp_03_desc, cmp_04_desc, cmp_05_desc, cmp_06_desc,
+                cmp_07_desc, cmp_08_desc, cmp_09_desc,
         };
 
         ag_test_suite *ctx = ag_test_suite_new("ag_str interface");
