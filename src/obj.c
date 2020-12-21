341d2934c49dfdb74d02adab91d26be9d65e1faa
Abhishek Chakravarti <abhishek@taranjali.org>
2020-12-21 10:50:51 +0530

    Add polymorphic support to object interface
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/obj.c
+++ src/obj.c
@@ -8,6 +8,12 @@ struct ag_obj {
 };
 
 
+static inline const struct ag_obj_vtable *vtable_get(const ag_obj *ctx)
+{
+        return ag_obj_registry_get(ctx->ob_typeid);
+}
+
+
 extern inline bool ag_obj_lt(const ag_obj *, const ag_obj *);
 extern inline bool ag_obj_eq(const ag_obj *, const ag_obj *);
 extern inline bool ag_obj_gt(const ag_obj *, const ag_obj *);
@@ -37,15 +43,20 @@ extern ag_obj *ag_obj_copy(const ag_obj *ctx)
 
 extern ag_obj *ag_obj_clone(const ag_obj *ctx)
 {
-        return ag_mblock_copy(ctx);
+        //return ag_mblock_copy(ctx);
+        //return vtable_get(ctx)->vt_clone(ctx);
+
+        return ag_obj_new(ctx->ob_typeid,
+                          vtable_get(ctx)->vt_clone(ctx->ob_payload));
 }
 
 
 extern void ag_obj_release(ag_obj **ctx)
 {
         ag_obj *o = *ctx;
-        ag_mblock *m = o;
+        vtable_get(o)->vt_release(o->ob_payload);
 
+        ag_mblock *m = o;
         ag_mblock_release(&m);
         *ctx = m;
 }
@@ -53,13 +64,15 @@ extern void ag_obj_release(ag_obj **ctx)
 
 extern enum ag_cmp ag_obj_cmp(const ag_obj *ctx, const ag_obj *cmp)
 {
-        return ag_mblock_cmp(ctx, cmp);
+        //return ag_mblock_cmp(ctx, cmp);
+        return vtable_get(ctx)->vt_cmp(ctx, cmp);
 }
 
 
 extern bool ag_obj_valid(const ag_obj *ctx)
 {
-        return ctx;
+        //return ctx;
+        return vtable_get(ctx)->vt_valid(ctx);
 }
 
 
@@ -77,19 +90,22 @@ extern size_t ag_obj_refc(const ag_obj *ctx)
 
 extern size_t ag_obj_len(const ag_obj *ctx)
 {
-        return 1;
+        //return 1;
+        return vtable_get(ctx)->vt_len(ctx);
 }
 
 
 extern size_t ag_obj_hash(const ag_obj *ctx)
 {
-        return 1;
+        //return 1;
+        return vtable_get(ctx)->vt_hash(ctx);
 }
 
 
 extern ag_str *ag_obj_str(const ag_obj *ctx)
 {
-        return ag_mblock_str(ctx);
+        //return ag_mblock_str(ctx);
+        return vtable_get(ctx)->vt_str(ctx);
 }
 
 
