383b8b3563ff89ddfbf5a218bd420d71a370d289
Abhishek Chakravarti <abhishek@taranjali.org>
2020-12-24 22:27:02 +0530

    Add test cases for ag_obj_copy()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/obj.c
+++ test/obj.c
@@ -173,6 +173,30 @@ AG_TEST_INIT(copy_04, "ag_obj_copy() updates the reference count of a shallow"
 } AG_TEST_EXIT();
 
 
+AG_TEST_INIT(copy_05, "ag_obj_copy() preserves the data of the shallow copy of"
+                      " a base object") {
+        AG_AUTO(ag_obj) *o  = sample_base();
+        AG_AUTO(ag_obj) *o2 = ag_obj_copy(o);
+
+        const struct payload_base *p  = ag_obj_payload(o);
+        const struct payload_base *p2 = ag_obj_payload(o2);
+
+        AG_TEST_ASSERT (p->x == p2->x && p->y == p2->y);
+} AG_TEST_EXIT();
+
+
+AG_TEST_INIT(copy_06, "ag_obj_copy() preserves the data of the shallow copy of"
+                      " a derived object") {
+        AG_AUTO(ag_obj) *o  = sample_derived();
+        AG_AUTO(ag_obj) *o2 = ag_obj_copy(o);
+
+        const struct payload_derived *p  = ag_obj_payload(o);
+        const struct payload_derived *p2 = ag_obj_payload(o2);
+
+        AG_TEST_ASSERT (*p->x == *p2->x && *p->y == *p2->y);
+} AG_TEST_EXIT();
+
+
 extern ag_test_suite *test_suite_obj(void)
 {
         register_base();
@@ -180,12 +204,12 @@ extern ag_test_suite *test_suite_obj(void)
 
         ag_test *test[] = {
                 new_01,  new_02,  copy_01, copy_02,
-                copy_03, copy_04,
+                copy_03, copy_04, copy_05, copy_06,
         };
 
         const char *desc[] = {
                 new_01_desc,  new_02_desc,  copy_01_desc, copy_02_desc,
-                copy_03_desc, copy_04_desc,
+                copy_03_desc, copy_04_desc, copy_05_desc, copy_06_desc,
         };
 
         ag_test_suite *ctx = ag_test_suite_new("ag_obj interface");
