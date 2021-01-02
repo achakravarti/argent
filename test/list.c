2e982e5fd83ad5605e9228a519d2f4267e10f857
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-02 21:30:46 +0530

    Add test cases for ag_list_lt()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/list.c
+++ test/list.c
@@ -18,16 +18,22 @@ static ag_list *sample_int(void)
 
 static ag_list *sample_int_2(void)
 {
-        AG_AUTO(ag_value) *v1 = ag_value_new_int(-666);
+        AG_AUTO(ag_value) *v1 = ag_value_new_int(-123);
         AG_AUTO(ag_value) *v2 = ag_value_new_int(0);
-        AG_AUTO(ag_value) *v3 = ag_value_new_int(555);
-        AG_AUTO(ag_value) *v4 = ag_value_new_int(734);
+        AG_AUTO(ag_value) *v3 = ag_value_new_int(456);
+        AG_AUTO(ag_value) *v4 = ag_value_new_int(-666);
+        AG_AUTO(ag_value) *v5 = ag_value_new_int(0);
+        AG_AUTO(ag_value) *v6 = ag_value_new_int(555);
+        AG_AUTO(ag_value) *v7 = ag_value_new_int(734);
 
         AG_AUTO(ag_list) *l = ag_list_new();
         ag_list_push(&l, v1);
         ag_list_push(&l, v2);
         ag_list_push(&l, v3);
         ag_list_push(&l, v4);
+        ag_list_push(&l, v5);
+        ag_list_push(&l, v6);
+        ag_list_push(&l, v7);
 
         return ag_list_copy(l);
 }
@@ -148,6 +154,42 @@ AG_TEST_INIT(release_03,
 AG_TEST_EXIT();
 
 
+AG_TEST_INIT(lt_01, 
+    "ag_list_lt() returns true if a list is lexicographically smaller than"
+    " another")
+{
+        AG_AUTO(ag_list) *l = sample_int();
+        AG_AUTO(ag_list) *l2 = sample_int_2();
+
+        AG_TEST_ASSERT (ag_list_lt(l, l2));
+}
+AG_TEST_EXIT();
+
+
+AG_TEST_INIT(lt_02, 
+    "ag_list_lt() returns false if a list is lexicographically equal to"
+     " another")
+{
+        AG_AUTO(ag_list) *l = sample_int();
+        AG_AUTO(ag_list) *l2 = sample_int();
+
+        AG_TEST_ASSERT (!ag_list_lt(l, l2));
+}
+AG_TEST_EXIT();
+
+
+AG_TEST_INIT(lt_03,
+    "ag_list_lt() returns false if a list is lexicographically greater than"
+    " another")
+{
+        AG_AUTO(ag_list) *l = sample_int_2();
+        AG_AUTO(ag_list) *l2 = sample_int();
+
+        AG_TEST_ASSERT (!ag_list_lt(l, l2));
+}
+AG_TEST_EXIT();
+
+
 extern ag_test_suite *test_suite_list(void)
 {
         ag_test *test[] = {
@@ -160,6 +202,8 @@ extern ag_test_suite *test_suite_list(void)
                 
                 release_01, release_02, release_03, 
                 release_04,
+
+                lt_01, lt_02, lt_03,
         };
 
         const char *desc[] = {
@@ -172,6 +216,9 @@ extern ag_test_suite *test_suite_list(void)
                 
                 release_01_desc, release_02_desc,
                 release_03_desc, release_04_desc,
+
+                lt_01_desc, lt_02_desc,
+                lt_03_desc,
         };
 
         ag_test_suite *ctx = ag_test_suite_new("ag_list interface");
