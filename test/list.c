1ba4d87d4f770a00f3753ae905f008ae209a4d21
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-02 18:21:51 +0530

    Add test cases for ag_list_copy()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/list.c
+++ test/list.c
@@ -33,20 +33,51 @@ static ag_list *sample_int_2(void)
 }
 
 
-AG_TEST_INIT(new_01, "ag_list_new() can create a new sample list") {
+AG_TEST_INIT(new_01, "ag_list_new() can create a new sample list")
+{
         AG_AUTO(ag_list) *l = sample_int();
         AG_TEST_ASSERT (l && ag_list_len(l) == 3);
-} AG_TEST_EXIT();
+}
+AG_TEST_EXIT();
+
+
+AG_TEST_INIT(new_02, "ag_list_new() can create a new empty list")
+{
+        AG_AUTO(ag_list) *l = ag_list_new();
+        AG_TEST_ASSERT (l && ag_list_empty(l));
+}
+AG_TEST_EXIT();
+
+
+AG_TEST_INIT(copy_01, "ag_list_copy() creates a shallow copy of a sample list")
+{
+        AG_AUTO(ag_list) *l = sample_int();
+        AG_AUTO(ag_list) *l2 = ag_list_copy(l);
+        AG_TEST_ASSERT (l2 == l);
+}
+AG_TEST_EXIT();
+
+
+AG_TEST_INIT(copy_02, "ag_list_copy() increases the reference count by 1")
+{
+        AG_AUTO(ag_list) *l = sample_int();
+        AG_AUTO(ag_list) *l2 = ag_list_copy(l);
+        AG_TEST_ASSERT (ag_list_refc(l) == 2);
+}
+AG_TEST_EXIT();
+
 
 
 extern ag_test_suite *test_suite_list(void)
 {
         ag_test *test[] = {
-                new_01,
+                new_01, new_02, copy_01,
+                copy_02,
         };
 
         const char *desc[] = {
-                new_01_desc,
+                new_01_desc, new_02_desc, copy_01_desc,
+                copy_02_desc,
         };
 
         ag_test_suite *ctx = ag_test_suite_new("ag_list interface");
