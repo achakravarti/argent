aac4848e79368d4e9428f197647aef8e7b296dcc
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-03 15:08:15 +0530

    Reformat list test cases
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/list.c
+++ test/list.c
@@ -387,74 +387,47 @@ AG_TEST_CASE(str_02,
 extern ag_test_suite *test_suite_list(void)
 {
         ag_test *test[] = {
-                new_01, new_02, 
-                
-                copy_01, copy_02, 
-
-                clone_01, clone_02, clone_03,
+                new_01,         new_02, 
+                copy_01,        copy_02, 
+                clone_01,       clone_02,       clone_03,
                 clone_04,
-                
-                release_01, release_02, release_03, 
+                release_01,     release_02,     release_03,
                 release_04,
-
-                lt_01, lt_02, lt_03,
-                
-                eq_01, eq_02, eq_03,
-                
-                gt_01, gt_02, gt_03,
-
-                empty_01, empty_02,
-
-                typeid_01, uuid_01,
-
-                valid_01, valid_02,
-
-                sz_01, sz_02,
-
-                refc_01, refc_02,
-
-                len_01, len_02,
-
-                hash_01, hash_02,
-
-                str_01, str_02,
+                lt_01,          lt_02,          lt_03,
+                eq_01,          eq_02,          eq_03,
+                gt_01,          gt_02,          gt_03,
+                empty_01,       empty_02,
+                typeid_01, 
+                uuid_01,
+                valid_01,       valid_02,
+                sz_01,          sz_02,
+                refc_01,        refc_02,
+                len_01,         len_02,
+                hash_01,        hash_02,
+                str_01,         str_02,
         };
 
         const char *desc[] = {
-                new_01_desc, new_02_desc, 
-                
-                copy_01_desc, copy_02_desc, 
-                
-                clone_01_desc, clone_02_desc, 
-                clone_03_desc, clone_04_desc,
-                
-                release_01_desc, release_02_desc,
-                release_03_desc, release_04_desc,
-
-                lt_01_desc, lt_02_desc,
+                new_01_desc,            new_02_desc, 
+                copy_01_desc,           copy_02_desc, 
+                clone_01_desc,          clone_02_desc, 
+                clone_03_desc,          clone_04_desc,
+                release_01_desc,        release_02_desc,
+                release_03_desc,        release_04_desc,
+                lt_01_desc,             lt_02_desc,
                 lt_03_desc,
-                
-                eq_01_desc, eq_02_desc,
+                eq_01_desc,             eq_02_desc,
                 eq_03_desc,
-                
-                gt_01_desc, gt_02_desc,
+                gt_01_desc,             gt_02_desc,
                 gt_03_desc,
-
-                empty_01_desc, empty_02_desc,
-
-                typeid_01_desc, uuid_01_desc,
-
-                valid_01_desc, valid_02_desc,
-
-                sz_01_desc, sz_02_desc,
-
-                refc_01_desc, refc_02_desc,
-
-                len_01_desc, len_02_desc,
-
-                hash_01_desc, hash_02_desc,
-
-                str_01_desc, str_02_desc,
+                empty_01_desc,          empty_02_desc,
+                typeid_01_desc,         uuid_01_desc,
+                valid_01_desc,          valid_02_desc,
+                sz_01_desc,             sz_02_desc,
+                refc_01_desc,           refc_02_desc,
+                len_01_desc,            len_02_desc,
+                hash_01_desc,           hash_02_desc,
+                str_01_desc,            str_02_desc,
         };
 
         ag_test_suite *ctx = ag_test_suite_new("ag_list interface");
