d9a914da3afc4628b7c39a0a574e2c8a6320ad8c
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-05 20:02:54 +0530

    Reformat alignments
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- include/list.h
+++ include/list.h
@@ -55,8 +55,8 @@ typedef void            (ag_list_iterator_mutable)(ag_value **, void *);
  */
 
 
-extern void __ag_list_register__(void);
-extern ag_list *ag_list_new(void);
+extern void      __ag_list_register__(void);
+extern ag_list  *ag_list_new(void);
 
 
 inline ag_list *
@@ -239,7 +239,7 @@ ag_list_str(const ag_list *ctx)
 
 extern ag_value *ag_list_get(const ag_list *);
 extern ag_value *ag_list_get_at(const ag_list *, size_t);
-extern void ag_list_map(const ag_list *, ag_list_iterator *, void *);
+extern void      ag_list_map(const ag_list *, ag_list_iterator *, void *);
 
 
 /*
@@ -250,12 +250,13 @@ extern void ag_list_map(const ag_list *, ag_list_iterator *, void *);
  */
 
 
-extern void ag_list_set(ag_list **, const ag_value *);
-extern void ag_list_set_at(ag_list **, const ag_value *, size_t);
-extern void ag_list_map_mutable(ag_list **, ag_list_iterator_mutable *, void *);
-extern void ag_list_start(ag_list **);
-extern bool ag_list_next(ag_list **);
-extern void ag_list_push(ag_list **, const ag_value *);
+extern void     ag_list_set(ag_list **, const ag_value *);
+extern void     ag_list_set_at(ag_list **, const ag_value *, size_t);
+extern void     ag_list_map_mutable(ag_list **, ag_list_iterator_mutable *,
+                    void *);
+extern void     ag_list_start(ag_list **);
+extern bool     ag_list_next(ag_list **);
+extern void     ag_list_push(ag_list **, const ag_value *);
 
 
 #ifdef __cplusplus
