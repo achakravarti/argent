740e9e75fb7f80ad16ae0e912fef606106459739
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-11 23:25:40 +0530

    Add test cases for ag_field_eq()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/field.c
+++ test/field.c
@@ -87,7 +87,7 @@ AG_TEST_CASE("ag_field_release() reduces the reference count by 1")
 {
         ag_field *f = sample_int_str_small();
         AG_AUTO(ag_field) *cp = ag_field_copy(f);
-        AG_AUTO(ag_list) *cp2 = ag_field_copy(cp);
+        AG_AUTO(ag_field) *cp2 = ag_field_copy(cp);
         ag_field_release(&f);
 
         AG_TEST (!f && ag_field_refc(cp2) == 2);
@@ -103,7 +103,7 @@ AG_TEST_CASE("ag_field_release() performs a no-op if passed NULL")
 
 AG_TEST_CASE("ag_field_release() performs a no-op if passed a handle to NULL")
 {
-        ag_list *f = NULL;
+        ag_field *f = NULL;
         ag_field_release(&f);
 
         AG_TEST (true);
@@ -112,10 +112,78 @@ AG_TEST_CASE("ag_field_release() performs a no-op if passed a handle to NULL")
 
 AG_TEST_CASE("ag_field_release() disposes a single instance of a field")
 {
-        ag_list *f = sample_int_str_small();
+        ag_field *f = sample_int_str_small();
         ag_field_release(&f);
 
         AG_TEST (!f);
+}
+
+
+/*
+ * Define the the test cases for ag_field_lt(). Note that since ag_field_lt() is
+ * a specialised case of ag_field_cmp(), it helps to test test ag_field_cmp()
+ * too.
+ */
+
+
+AG_TEST_CASE("ag_field_lt() returns true if a field is less than another")
+{
+        AG_AUTO(ag_field) *f = sample_int_str_small();
+        AG_AUTO(ag_field) *f2 = sample_int_str_large();
+
+        AG_TEST (ag_field_lt(f, f2));
+}
+
+
+AG_TEST_CASE("ag_field_lt() returns false if a field is equal to another")
+{
+        AG_AUTO(ag_field) *f = sample_int_str_small();
+        AG_AUTO(ag_field) *f2 = sample_int_str_small();
+
+        AG_TEST (!ag_field_lt(f, f2));
+}
+
+
+AG_TEST_CASE("ag_field_lt() returns false if a field is greater than another")
+{
+        AG_AUTO(ag_field) *f = sample_int_str_large();
+        AG_AUTO(ag_field) *f2 = sample_int_str_small();
+
+        AG_TEST (!ag_field_lt(f, f2));
+}
+
+
+/*
+ * Define the test cases for ag_field_eq(). Since ag_field_eq() is a specialised
+ * case of ag_field_cmp(), this these test cases also indirectly help test out
+ * ag_field_cmp().
+ */
+
+
+AG_TEST_CASE("ag_field_eq() returns true if two fields are equal")
+{
+        AG_AUTO(ag_field) *f = sample_int_str_small();
+        AG_AUTO(ag_field) *f2 = sample_int_str_small();
+
+        AG_TEST (ag_field_eq(f, f2));
+}
+
+
+AG_TEST_CASE("ag_field_eq() returns false if a field is less than another")
+{
+        AG_AUTO(ag_field) *f = sample_int_str_small();
+        AG_AUTO(ag_field) *f2 = sample_int_str_large();
+
+        AG_TEST (!ag_field_eq(f, f2));
+}
+
+
+AG_TEST_CASE("ag_field_eq() returns false if a field is greater than another")
+{
+        AG_AUTO(ag_field) *f = sample_int_str_large();
+        AG_AUTO(ag_field) *f2 = sample_int_str_small();
+
+        AG_TEST (!ag_field_eq(f2, f));
 }
 
 
