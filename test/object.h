e736cfc46a3616d7b64d3b97f59d9802ef3b210b
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-12 13:01:18 +0530

    Define AG_TESTS_OBJECT_RELEASE() meta test
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/object.h
+++ test/object.h
@@ -44,5 +44,33 @@
         }
 
 
+#define AG_TESTS_OBJECT_RELEASE(type, sample)                           \
+        AG_TEST_CASE(#type "_release(): pptr NULL => no-op")            \
+        {                                                               \
+                type ## _release(NULL);                                 \
+                AG_TEST (true);                                         \
+        }                                                               \
+        AG_TEST_CASE(#type "_release(): pptr ~NULL ptr NULL => no-op")  \
+        {                                                               \
+                AG_AUTO(type) *o = NULL;                                \
+                type ## _release(&o);                                   \
+                AG_TEST (true);                                         \
+        }                                                               \
+        AG_TEST_CASE(#type "_release(): " #sample " => dispose ptr")    \
+        {                                                               \
+                type *o = sample;                                       \
+                type ## _release(&o);                                   \
+                AG_TEST (!o);                                           \
+        }                                                               \
+        AG_TEST_CASE(#type "_release(): " #sample " => refc -1")        \
+        {                                                               \
+                AG_AUTO(type) *o = sample;                              \
+                AG_AUTO(type) *cp = type ## _copy(o);                   \
+                type *cp2 = type ## _copy(cp);                          \
+                type ## _release(&cp2);                                 \
+                AG_TEST (type ## _refc(o) == 2);                        \
+        }
+
+
 #endif /* !__ARGENT_TESTS_OBJECT_H__ */
 
