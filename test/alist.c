ce1ff834f65efb859495748f69fd281a3b983863
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-14 09:28:04 +0530

    Define and use VALUE_STR(), VALUE_INT() and FIELD() along with alist metatests
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/alist.c
+++ test/alist.c
@@ -1,8 +1,54 @@
-#include "./test.h"
+#include "./alist.h"
 #include "./object.h"
+#include "./test.h"
 
 
 #define __AG_TEST_SUITE_ID__ 7
+
+
+/* Generates function to return a sample string value */
+#define VALUE_STR(tag, str)                                     \
+        static inline ag_value *VALUE_STR_ ## tag(void)         \
+        {                                                       \
+                AG_AUTO(ag_string) *s = ag_string_new(str);     \
+                return ag_value_new_string(s);                  \
+        }
+
+
+/* Generates function to return a sample int value */
+#define VALUE_INT(tag, i)                               \
+        static inline ag_value *VALUE_INT_ ## tag(void) \
+        {                                               \
+                return ag_value_new_int(i);             \
+        }
+
+/* Generate function to return a sample field */
+#define FIELD(tag, key, val)                            \
+        static inline ag_field *FIELD_ ## tag(void)     \
+        {                                               \
+                AG_AUTO(ag_value) *k = key;             \
+                AG_AUTO(ag_value) *v = val;             \
+                return ag_field_new(k, v);              \
+        }
+
+
+VALUE_STR(BAR, "bar");
+VALUE_STR(FOO, "foo");
+VALUE_STR(FOOBAR, "foobar");
+VALUE_STR(KEY, "key");
+VALUE_STR(VAL, "val");
+
+
+VALUE_INT(1, 1);
+VALUE_INT(2, 2);
+VALUE_INT(3, 3);
+VALUE_INT(4, 4);
+
+
+FIELD(KEY_VAL, VALUE_STR_KEY(), VALUE_STR_VAL());
+FIELD(1_FOO, VALUE_INT_1(), VALUE_STR_FOO());
+FIELD(2_BAR, VALUE_INT_2(), VALUE_STR_BAR());
+FIELD(3_FOOBAR, VALUE_INT_3(), VALUE_STR_FOOBAR());
 
 
 static ag_alist *sample_empty(void);
@@ -65,12 +111,15 @@ AG_TEST_OBJECT_LEN(ag_alist, sample_list(), 3);
 AG_TEST_OBJECT_STR_HAS(ag_alist, sample_empty(), "list");
 
 
+AG_METATEST_ALIST_HAS_NOT(sample_empty(), FIELD_KEY_VAL());
+
+
 extern ag_test_suite *test_suite_alist(void)
 {
         return AG_TEST_SUITE_GENERATE("ag_alist interface");
 }
 
-
+        
 static ag_alist *
 sample_empty(void)
 {
@@ -81,35 +130,19 @@ sample_empty(void)
 static ag_alist *
 sample_single(void)
 {
-        AG_AUTO(ag_string) *k = ag_string_new("key");
-        AG_AUTO(ag_string) *v = ag_string_new("val");
-        
-        AG_AUTO(ag_value) *kv = ag_value_new_string(k);
-        AG_AUTO(ag_value) *vv = ag_value_new_string(v);
-
-        AG_AUTO(ag_field) *f = ag_field_new(kv, vv);
+        AG_AUTO(ag_field) *f = FIELD_KEY_VAL();
         return ag_alist_new(f);
 }
+
+
 
 
 static ag_alist *
 sample_list(void)
 {
-        AG_AUTO(ag_string) *v1 = ag_string_new("foo");
-        AG_AUTO(ag_string) *v2 = ag_string_new("bar");
-        AG_AUTO(ag_string) *v3 = ag_string_new("foobar");
-
-        AG_AUTO(ag_value) *vv1 = ag_value_new_string(v1);
-        AG_AUTO(ag_value) *vv2 = ag_value_new_string(v2);
-        AG_AUTO(ag_value) *vv3 = ag_value_new_string(v3);
-
-        AG_AUTO(ag_value) *kv1 = ag_value_new_int(1);
-        AG_AUTO(ag_value) *kv2 = ag_value_new_int(2);
-        AG_AUTO(ag_value) *kv3 = ag_value_new_int(3);
-
-        AG_AUTO(ag_field) *f1 = ag_field_new(kv1, vv1);
-        AG_AUTO(ag_field) *f2 = ag_field_new(kv2, vv2);
-        AG_AUTO(ag_field) *f3 = ag_field_new(kv3, vv3);
+        AG_AUTO(ag_field) *f1 = FIELD_1_FOO();
+        AG_AUTO(ag_field) *f2 = FIELD_2_BAR();
+        AG_AUTO(ag_field) *f3 = FIELD_3_FOOBAR();
 
         const ag_field *f[] = {f1, f2, f3};
         return ag_alist_new_array(f, 3);
@@ -119,17 +152,13 @@ sample_list(void)
 static ag_alist *
 sample_list_2(void)
 {
-        AG_AUTO(ag_string) *v1 = ag_string_new("foo");
-        AG_AUTO(ag_string) *v2 = ag_string_new("bar");
-        AG_AUTO(ag_string) *v3 = ag_string_new("foobar");
+        AG_AUTO(ag_value) *vv1 = VALUE_STR_FOO();
+        AG_AUTO(ag_value) *vv2 = VALUE_STR_BAR();
+        AG_AUTO(ag_value) *vv3 = VALUE_STR_FOOBAR();
 
-        AG_AUTO(ag_value) *vv1 = ag_value_new_string(v1);
-        AG_AUTO(ag_value) *vv2 = ag_value_new_string(v2);
-        AG_AUTO(ag_value) *vv3 = ag_value_new_string(v3);
-
-        AG_AUTO(ag_value) *kv1 = ag_value_new_int(2);
-        AG_AUTO(ag_value) *kv2 = ag_value_new_int(3);
-        AG_AUTO(ag_value) *kv3 = ag_value_new_int(4);
+        AG_AUTO(ag_value) *kv1 = VALUE_INT_2();
+        AG_AUTO(ag_value) *kv2 = VALUE_INT_3();
+        AG_AUTO(ag_value) *kv3 = VALUE_INT_4();
 
         AG_AUTO(ag_field) *f1 = ag_field_new(kv1, vv1);
         AG_AUTO(ag_field) *f2 = ag_field_new(kv2, vv2);
