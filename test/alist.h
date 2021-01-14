240ad74c4197b28bbec802a8cc6827c4157ebc62
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-14 10:28:07 +0530

    Define metatests for ag_alist_has_key()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/alist.h
+++ test/alist.h
@@ -3,7 +3,7 @@
 
 
 #define AG_METATEST_ALIST_HAS(sample, attr)                             \
-        AG_TEST_CASE("ag_alist_has(): " #sample " <- "#attr " => T")    \
+        AG_TEST_CASE("ag_alist_has(): " #sample " <- " #attr " => T")   \
         {                                                               \
                 AG_AUTO(ag_alist) *a = sample;                          \
                 AG_AUTO(ag_field) *f = attr;                            \
@@ -12,11 +12,30 @@
 
 
 #define AG_METATEST_ALIST_HAS_NOT(sample, attr)                         \
-        AG_TEST_CASE("ag_alist_has(): " #sample " <- "#attr " => F")    \
+        AG_TEST_CASE("ag_alist_has(): " #sample " <- " #attr " => F")   \
         {                                                               \
                 AG_AUTO(ag_alist) *a = sample;                          \
                 AG_AUTO(ag_field) *f = attr;                            \
                 AG_TEST (!ag_alist_has(a, f));                          \
         }
+
+
+#define AG_METATEST_ALIST_HAS_KEY(sample, key)                          \
+        AG_TEST_CASE("ag_alist_has(): " #sample " <- " #key " => T")    \
+        {                                                               \
+                AG_AUTO(ag_alist) *a = sample;                          \
+                AG_AUTO(ag_value) *k = key;                             \
+                AG_TEST (ag_alist_has(a, k));                           \
+        }
+
+
+#define AG_METATEST_ALIST_HAS_KEY_NOT(sample, key)                      \
+        AG_TEST_CASE("ag_alist_has(): " #sample " <- " #key " => F")    \
+        {                                                               \
+                AG_AUTO(ag_alist) *a = sample;                          \
+                AG_AUTO(ag_value) *k = key;                             \
+                AG_TEST (!ag_alist_has(a, k));                          \
+        }
+
 
 #endif /* !__AG_ARGENT_METATESTS_ALIST_H__ */
