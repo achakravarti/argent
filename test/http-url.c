7293d05712e7fdcb4c4fd0b5986371596790dd69
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-18 09:50:08 +0530

    Add object meta-tests for ag_http_url
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/http-url.c
+++ test/http-url.c
@@ -24,7 +24,80 @@ static inline ag_http_url *sample_2(void)
 }
 
 
+static inline size_t len_sample(void)
+{
+        AG_AUTO(ag_http_url) *u = sample();
+        AG_AUTO(ag_string) *s = ag_http_url_str(u);
+        
+        return ag_string_len(s);
+}
+
+
+static inline size_t len_sample_2(void)
+{
+        AG_AUTO(ag_http_url) *u = sample_2();
+        AG_AUTO(ag_string) *s = ag_http_url_str(u);
+        
+        return ag_string_len(s);
+}
+
+
+static inline size_t sz_sample(void)
+{
+        AG_AUTO(ag_http_url) *u = sample();
+        AG_AUTO(ag_string) *s = ag_http_url_str(u);
+        
+        return ag_string_sz(s);
+}
+
+
+static inline size_t sz_sample_2(void)
+{
+        AG_AUTO(ag_http_url) *u = sample_2();
+        AG_AUTO(ag_string) *s = ag_http_url_str(u);
+        
+        return ag_string_sz(s);
+}
+
+
 AG_METATEST_OBJECT_COPY(ag_http_url, sample());
+AG_METATEST_OBJECT_COPY(ag_http_url, sample_2());
+
+AG_METATEST_OBJECT_CLONE(ag_http_url, sample());
+AG_METATEST_OBJECT_CLONE(ag_http_url, sample_2());
+
+AG_METATEST_OBJECT_RELEASE(ag_http_url, sample());
+AG_METATEST_OBJECT_RELEASE(ag_http_url, sample_2());
+
+
+AG_METATEST_OBJECT_CMP(ag_http_url, sample(), sample_2());
+AG_METATEST_OBJECT_LT(ag_http_url, sample(), sample_2());
+AG_METATEST_OBJECT_EQ(ag_http_url, sample(), sample_2());
+AG_METATEST_OBJECT_GT(ag_http_url, sample(), sample_2());
+
+
+AG_METATEST_OBJECT_EMPTY_NOT(ag_http_url, sample());
+AG_METATEST_OBJECT_EMPTY_NOT(ag_http_url, sample_2());
+
+
+AG_METATEST_OBJECT_TYPEID(ag_http_url, sample(), AG_TYPEID_HTTP_URL);
+AG_METATEST_OBJECT_TYPEID(ag_http_url, sample_2(), AG_TYPEID_HTTP_URL);
+
+
+AG_METATEST_OBJECT_UUID(ag_http_url, sample());
+AG_METATEST_OBJECT_UUID(ag_http_url, sample_2());
+
+
+AG_METATEST_OBJECT_REFC(ag_http_url, sample());
+AG_METATEST_OBJECT_REFC(ag_http_url, sample_2());
+
+
+AG_METATEST_OBJECT_LEN(ag_http_url, sample(), len_sample());
+AG_METATEST_OBJECT_LEN(ag_http_url, sample_2(), len_sample_2());
+
+
+AG_METATEST_OBJECT_SZ(ag_http_url, sample(), sz_sample());
+AG_METATEST_OBJECT_SZ(ag_http_url, sample_2(), sz_sample_2());
 
 
 extern ag_test_suite *
