6327fa1e6e542beee50ed8f32febf1d4fa908a25
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-18 10:39:39 +0530

    Generate support functions for sample URLs
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/http-url.c
+++ test/http-url.c
@@ -4,100 +4,79 @@
 
 #define __AG_TEST_SUITE_ID__ 8
 
-/*
-HTTP_LOCALHOST_8080_FOO_BAR_HTHhttp://localhost:8080/foo/bar.html
-https://localhost:8080/foo/bar.html
-*/
+
+#define SAMPLE(tag, secure, host, port, path)                           \
+        static inline ag_http_url *tag(void)                            \
+        {                                                               \
+                return ag_http_url_new(secure, host, port, path);       \
+        }                                                               \
+        static inline size_t tag ## _LEN(void)                          \
+        {                                                               \
+                AG_AUTO(ag_http_url) *u = tag();                        \
+                AG_AUTO(ag_string) *s = ag_http_url_str(u);             \
+                return ag_string_len(s);                                \
+        }                                                               \
+        static inline size_t tag ## _SZ(void)                           \
+        {                                                               \
+                AG_AUTO(ag_http_url) *u = tag();                        \
+                AG_AUTO(ag_string) *s = ag_http_url_str(u);             \
+                return ag_string_sz(s);                                 \
+        }
 
 
-static inline ag_http_url *sample(void)
-{
-        return ag_http_url_new(false, "localhost", "8080",
-            "foo/bar.html");
-}
+SAMPLE(HTTP_LOCALHOST_8080_NOPATH, false, "localhost", 8080, "");
+SAMPLE(HTTPS_LOCALHOST_8080_NOPATH, true, "localhost", 8080, "");
 
 
-static inline ag_http_url *sample_2(void)
-{
-        return ag_http_url_new(true, "localhost", "8080",
-            "foo/bar.html");
-}
+AG_METATEST_OBJECT_COPY(ag_http_url, HTTP_LOCALHOST_8080_NOPATH());
+AG_METATEST_OBJECT_COPY(ag_http_url, HTTPS_LOCALHOST_8080_NOPATH());
+
+AG_METATEST_OBJECT_CLONE(ag_http_url, HTTP_LOCALHOST_8080_NOPATH());
+AG_METATEST_OBJECT_CLONE(ag_http_url, HTTPS_LOCALHOST_8080_NOPATH());
+
+AG_METATEST_OBJECT_RELEASE(ag_http_url, HTTP_LOCALHOST_8080_NOPATH());
+AG_METATEST_OBJECT_RELEASE(ag_http_url, HTTPS_LOCALHOST_8080_NOPATH());
 
 
-static inline size_t len_sample(void)
-{
-        AG_AUTO(ag_http_url) *u = sample();
-        AG_AUTO(ag_string) *s = ag_http_url_str(u);
-        
-        return ag_string_len(s);
-}
+AG_METATEST_OBJECT_CMP(ag_http_url, HTTP_LOCALHOST_8080_NOPATH(),
+    HTTPS_LOCALHOST_8080_NOPATH());
+AG_METATEST_OBJECT_LT(ag_http_url, HTTP_LOCALHOST_8080_NOPATH(),
+    HTTPS_LOCALHOST_8080_NOPATH());
+AG_METATEST_OBJECT_EQ(ag_http_url, HTTP_LOCALHOST_8080_NOPATH(),
+    HTTPS_LOCALHOST_8080_NOPATH());
+AG_METATEST_OBJECT_GT(ag_http_url, HTTP_LOCALHOST_8080_NOPATH(),
+    HTTPS_LOCALHOST_8080_NOPATH());
 
 
-static inline size_t len_sample_2(void)
-{
-        AG_AUTO(ag_http_url) *u = sample_2();
-        AG_AUTO(ag_string) *s = ag_http_url_str(u);
-        
-        return ag_string_len(s);
-}
+AG_METATEST_OBJECT_EMPTY_NOT(ag_http_url, HTTP_LOCALHOST_8080_NOPATH());
+AG_METATEST_OBJECT_EMPTY_NOT(ag_http_url, HTTPS_LOCALHOST_8080_NOPATH());
 
 
-static inline size_t sz_sample(void)
-{
-        AG_AUTO(ag_http_url) *u = sample();
-        AG_AUTO(ag_string) *s = ag_http_url_str(u);
-        
-        return ag_string_sz(s);
-}
+AG_METATEST_OBJECT_TYPEID(ag_http_url, HTTP_LOCALHOST_8080_NOPATH(),
+    AG_TYPEID_HTTP_URL);
+AG_METATEST_OBJECT_TYPEID(ag_http_url, HTTPS_LOCALHOST_8080_NOPATH(),
+    AG_TYPEID_HTTP_URL);
 
 
-static inline size_t sz_sample_2(void)
-{
-        AG_AUTO(ag_http_url) *u = sample_2();
-        AG_AUTO(ag_string) *s = ag_http_url_str(u);
-        
-        return ag_string_sz(s);
-}
+AG_METATEST_OBJECT_UUID(ag_http_url, HTTP_LOCALHOST_8080_NOPATH());
+AG_METATEST_OBJECT_UUID(ag_http_url, HTTPS_LOCALHOST_8080_NOPATH());
 
 
-AG_METATEST_OBJECT_COPY(ag_http_url, sample());
-AG_METATEST_OBJECT_COPY(ag_http_url, sample_2());
-
-AG_METATEST_OBJECT_CLONE(ag_http_url, sample());
-AG_METATEST_OBJECT_CLONE(ag_http_url, sample_2());
-
-AG_METATEST_OBJECT_RELEASE(ag_http_url, sample());
-AG_METATEST_OBJECT_RELEASE(ag_http_url, sample_2());
+AG_METATEST_OBJECT_REFC(ag_http_url, HTTP_LOCALHOST_8080_NOPATH());
+AG_METATEST_OBJECT_REFC(ag_http_url, HTTPS_LOCALHOST_8080_NOPATH());
 
 
-AG_METATEST_OBJECT_CMP(ag_http_url, sample(), sample_2());
-AG_METATEST_OBJECT_LT(ag_http_url, sample(), sample_2());
-AG_METATEST_OBJECT_EQ(ag_http_url, sample(), sample_2());
-AG_METATEST_OBJECT_GT(ag_http_url, sample(), sample_2());
+AG_METATEST_OBJECT_LEN(ag_http_url, HTTP_LOCALHOST_8080_NOPATH(),
+    HTTP_LOCALHOST_8080_NOPATH_LEN());
+AG_METATEST_OBJECT_LEN(ag_http_url, HTTPS_LOCALHOST_8080_NOPATH(),
+    HTTPS_LOCALHOST_8080_NOPATH_LEN());
 
 
-AG_METATEST_OBJECT_EMPTY_NOT(ag_http_url, sample());
-AG_METATEST_OBJECT_EMPTY_NOT(ag_http_url, sample_2());
+AG_METATEST_OBJECT_SZ(ag_http_url, HTTP_LOCALHOST_8080_NOPATH(),
+    HTTP_LOCALHOST_8080_NOPATH_SZ());
+AG_METATEST_OBJECT_SZ(ag_http_url, HTTPS_LOCALHOST_8080_NOPATH(),
+    HTTPS_LOCALHOST_8080_NOPATH_SZ());
 
-
-AG_METATEST_OBJECT_TYPEID(ag_http_url, sample(), AG_TYPEID_HTTP_URL);
-AG_METATEST_OBJECT_TYPEID(ag_http_url, sample_2(), AG_TYPEID_HTTP_URL);
-
-
-AG_METATEST_OBJECT_UUID(ag_http_url, sample());
-AG_METATEST_OBJECT_UUID(ag_http_url, sample_2());
-
-
-AG_METATEST_OBJECT_REFC(ag_http_url, sample());
-AG_METATEST_OBJECT_REFC(ag_http_url, sample_2());
-
-
-AG_METATEST_OBJECT_LEN(ag_http_url, sample(), len_sample());
-AG_METATEST_OBJECT_LEN(ag_http_url, sample_2(), len_sample_2());
-
-
-AG_METATEST_OBJECT_SZ(ag_http_url, sample(), sz_sample());
-AG_METATEST_OBJECT_SZ(ag_http_url, sample_2(), sz_sample_2());
 
 
 extern ag_test_suite *
