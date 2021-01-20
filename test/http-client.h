feef73de69ad062f604230082aa7138408f1d8d3
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-20 19:12:58 +0530

    Refactor ag_http_client string metatests
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/http-client.h
+++ test/http-client.h
@@ -2,7 +2,42 @@
 #define __AG_HTTP_CLIENT_METATESTS_H__
 
 
+#define __AG_METATEST_HTTP_CLIENT_STR__(tag, sample, expect)                   \
+        AG_TEST_CASE("ag_http_client_" #tag "(): " #sample " => " expect)      \
+        {                                                                      \
+                AG_AUTO(ag_http_client) *c = sample;                           \
+                AG_AUTO(ag_string) *s = ag_http_client_ ## tag ## (c);         \
+                AG_TEST (ag_string_eq(s, expect));                             \
+        }
 
+
+#define AG_METATEST_HTTP_CLIENT_IP(sample, expect)              \
+        __AG_METATEST_HTTP_CLIENT_STR__(ip, sample, expect)
+
+
+#define AG_METATEST_HTTP_CLIENT_PORT(sample, expect)                    \
+        AG_TEST_CASE("ag_http_url_client(): " #sample " => " #expect)   \
+        {                                                               \
+                AG_AUTO(ag_http_client) *c = sample;                    \
+                AG_TEST (ag_http_client_port(u) == expect);             \
+        }
+
+
+#define AG_METATEST_HTTP_CLIENT_HOST(sample, expect)            \
+        __AG_METATEST_HTTP_CLIENT_STR__(host, sample, expect)
+
+
+#define AG_METATEST_HTTP_CLIENT_AGENT(sample, expect)           \
+        __AG_METATEST_HTTP_CLIENT_STR__(agent, sample, expect)
+
+
+#define AG_METATEST_HTTP_CLIENT_REFERER(sample, expect)         \
+        __AG_METATEST_HTTP_CLIENT_STR__(referer, sample, expect)
+
+
+
+
+#if 0
 #define AG_METATEST_HTTP_CLIENT_IP(sample, expect)                      \
         AG_TEST_CASE("ag_http_client_ip(): " #sample " => " expect)     \
         {                                                               \
@@ -45,6 +80,7 @@
                 AG_AUTO(ag_string) *r = ag_http_client_referer(c);             \
                 AG_TEST (ag_string_eq(r, expect));                             \
         }
+#endif
 
 
 #endif /* !__AG_HTTP_CLIENT_METATESTS_H__ */
