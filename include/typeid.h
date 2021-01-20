d0b911e4d29e9c2da5f30bac700d35f4f61c015d
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-20 11:58:19 +0530

    Define AG_TYPEID_HTTP_CLIENT
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- include/typeid.h
+++ include/typeid.h
@@ -14,6 +14,7 @@ typedef int ag_typeid;
 #define AG_TYPEID_LIST          ((ag_typeid) -2)
 #define AG_TYPEID_ALIST         ((ag_typeid) -3)
 #define AG_TYPEID_HTTP_URL      ((ag_typeid) -4)
+#define AG_TYPEID_HTTP_CLIENT   ((ag_typeid) -5)
 
 
 #ifdef __cplusplus
