04bfec0ed8a3a0e5e6931dca8c2bb6e35178b958
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-25 14:50:46 +0530

    Register ag_http_request object
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/manager.c
+++ src/manager.c
@@ -47,6 +47,7 @@ ag_init(void)
         AG_OBJECT_REGISTER(ag_alist);
         AG_OBJECT_REGISTER(ag_http_url);
         AG_OBJECT_REGISTER(ag_http_client);
+        AG_OBJECT_REGISTER(ag_http_request);
 }
 
 
