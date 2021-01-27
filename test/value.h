fe147e72244ec5f4f5d3ff8c914a46bfd9e9c465
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-27 20:02:09 +0530

    Add metafunctions to generate sample values
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ test/value.h
@@ -0,0 +1,43 @@
+#ifndef __AG_TEST_VALUE_H__
+#define __AG_TEST_VALUE_H__
+
+
+#define AG_SAMPLE_VALUE_INT(tag, val)                           \
+        static inline ag_value *SAMPLE_VALUE_ ## tag(void)      \
+        {                                                       \
+                return ag_value_new_int(val);                   \
+        }
+
+
+#define AG_SAMPLE_VALUE_UINT(tag, val)                          \
+        static inline ag_value *SAMPLE_VALUE_ ## tag(void)      \
+        {                                                       \
+                return ag_value_new_uint(val);                  \
+        }
+
+
+#define AG_SAMPLE_VALUE_FLOAT(tag, val)                         \
+        static inline ag_value *SAMPLE_VALUE_ ## tag(void)      \
+        {                                                       \
+                return ag_value_new_float(val);                 \
+        }
+
+
+#define AG_SAMPLE_VALUE_STRING(tag, val)                        \
+        static inline ag_value *SAMPLE_VALUE_ ## tag(void)      \
+        {                                                       \
+                AG_AUTO(ag_string) *s = ag_string_new(val);     \
+                return ag_value_new_string(s);                  \
+        }
+
+
+#define AG_SAMPLE_VALUE_OBJECT(tag, type, val)                  \
+        static inline ag_value *SAMPLE_VALUE_ ## tag(void)      \
+        {                                                       \
+                AG_AUTO(type) *o = val;                         \
+                return ag_value_new_object(val);                \
+        }
+
+
+#endif /* !__AG_TEST_VALUE_H__ */
+
