121e74ff6ea4d1847662a48dca789f531f6b3f42
Abhishek Chakravarti <abhishek@taranjali.org>
2021-01-31 19:46:54 +0530

    Define AG_SAMPLE_HTTP_REQUEST() macro
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/http-request.h
+++ test/http-request.h
@@ -25,4 +25,24 @@
 #define __ARGENT_TEST_HTTP_REQUEST_H__
 
 
+/*
+ * Define the AG_SAMPLE_HTTP_REQUESTT() macro. This macro is responsible for
+ * metaprogrammatically generating a static inline function that returns a
+ * pointer to a sample HTTP request object with a given set of properties.
+ *
+ * The first parameter to this macro is a tag used to uniquely identify a given
+ * sample HTTP client object, and the other parameters are, in sequence, those
+ * of ag_http_request_new(). The name of the generated function is the same as
+ * the specified tag.
+ */
+#define AG_SAMPLE_HTTP_REQUEST(tag, method, mime, url, client, param)   \
+        static inline ag_http_request *tag(void)                        \
+        {                                                               \
+                AG_AUTO(ag_http_url) *u = url;                          \
+                AG_AUTO(ag_http_client) *c = client;                    \
+                AG_AUTO(ag_alist) *p = param;                           \
+                return ag_http_request_new(method, mime, u, c, p);      \
+        }
+
+
 #endif /* !__AG_HTTP_REQUEST_H__ */
