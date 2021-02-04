07b57222461c90495b6e0750ca3286d35deda85e
Abhishek Chakravarti <abhishek@taranjali.org>
2021-02-04 15:29:16 +0530

    Add tests for ag_http_response_valid()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/http-response.c
+++ test/http-response.c
@@ -38,6 +38,10 @@ AG_METATEST_OBJECT_GT(ag_http_response, JSON_201(), HTML_200_EMPTY());
 
 AG_METATEST_OBJECT_EMPTY(ag_http_response, HTML_200_EMPTY());
 AG_METATEST_OBJECT_EMPTY_NOT(ag_http_response, JSON_201());
+
+
+AG_METATEST_OBJECT_VALID(ag_http_response, HTML_200_EMPTY());
+AG_METATEST_OBJECT_VALID(ag_http_response, JSON_201());
         
 
 
