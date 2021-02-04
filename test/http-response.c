23a5a8006cba2e06929663c50f021b7a49fec56f
Abhishek Chakravarti <abhishek@taranjali.org>
2021-02-04 14:57:14 +0530

    Define sample ag_http_response objects
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/http-response.c
+++ test/http-response.c
@@ -1,4 +1,4 @@
-#include "./http.h"
+#include "./http.h" 
 #include "./object.h"
 #include "./test.h"
 
@@ -7,9 +7,27 @@
 
 #define __AG_TEST_SUITE_ID__ 13
 
+
+
+
+AG_SAMPLE_HTTP_RESPONSE_EMPTY(HTML_200_EMPTY, AG_HTTP_MIME_TEXT_HTML,
+    AG_HTTP_STATUS_200_OK);
+AG_SAMPLE_HTTP_RESPONSE(JSON_201, AG_HTTP_MIME_APPLICATION_JSON,
+    AG_HTTP_STATUS_201_CREATED, "{key:foo, val:bar}");
+
+
+
+
+//AG_METATEST_OBJECT_COPY(ag_http_response, HTML_200_EMPTY());
+//AG_METATEST_OBJECT_COPY(ag_http_response, JSON_201());
+
+
+//AG_METATEST_OBJECT_CLONE(ag_http_response, HTML_200_EMPTY());
+//AG_METATEST_OBJECT_CLONE(ag_http_response, JSON_201());
         
-        
-        
+
+
+
 extern ag_test_suite *
 test_suite_http_response(void)
 {
