84c067d93d28a352092c2f3c130dccaefbe8dc8d
Abhishek Chakravarti <abhishek@taranjali.org>
2021-02-06 10:02:22 +0530

    Reformat
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- include/http.h
+++ include/http.h
@@ -21,8 +21,6 @@
  ******************************************************************************/
 
 
-
-
 #ifndef __ARGENT_INCLUDE_HTTP_H__
 #define __ARGENT_INCLUDE_HTTP_H__
 
@@ -34,9 +32,7 @@ extern "C" {
 #include "./object.h"
 
 
-
-
-/*******************************************************************************
+/**
  * The ag_http_method enumeration defines the common HTTP methods (verbs) used
  * to make HTTP requests. We have also declared the prototypes of two supporting
  * functions, ag_http_method_parse() and ag_http_method_str(), which are used to
@@ -45,7 +41,7 @@ extern "C" {
  * See the following files for more details:
  *   - src/http-enum.c
  *   - test/http-method.c
- */
+ **/
 
 enum ag_http_method {
         AG_HTTP_METHOD_GET,     /* GET    */
@@ -59,9 +55,7 @@ extern enum ag_http_method       ag_http_method_parse(const char *);
 extern ag_string                *ag_http_method_str(enum ag_http_method);
 
 
-
-
-/*******************************************************************************
+/**
  * The ag_http_mime enumeration represents the common HTTP MIME types that are
  * used most often. As of now, this enumeration is only a subset of all possible
  * MIME types, and may be expanded in future if required.
@@ -77,7 +71,7 @@ extern ag_string                *ag_http_method_str(enum ag_http_method);
  * See the following files for more details:
  *   - src/http-enum.c
  *   - test/http-mime.c
- */
+ **/
 
 enum ag_http_mime {
         AG_HTTP_MIME_APPLICATION_FORM,  /* application/x-www-form-urlencoded */
@@ -97,9 +91,7 @@ extern enum ag_http_mime         ag_http_mime_parse(const char *);
 extern ag_string                *ag_http_mime_str(enum ag_http_mime);
 
 
-
-
-/*******************************************************************************
+/**
  * The ag_http_status enumeration represents the commonly used subset of HTTP
  * response codes. Even from this subset, we anticipate that most would be
  * unused; however, they have been provided in case required by client code.
@@ -111,7 +103,7 @@ extern ag_string                *ag_http_mime_str(enum ag_http_mime);
  * See the following files for more details:
  *   - src/http-enum.c
  *   - test/http-status.c
- */
+ **/
 
 enum ag_http_status {
         AG_HTTP_STATUS_200_OK,                          /* 200 (OK)           */
@@ -147,9 +139,7 @@ extern enum ag_http_status       ag_http_status_parse(const char *);
 extern ag_string                *ag_http_status_str(enum ag_http_status);
 
 
-
-
-/*******************************************************************************
+/**
  * Declare the HTTP URL interface. An HTTP URL is the URL of the resource
  * requested by the client to the server, and makes up part of the HTTP request
  * specification. The Argent Library reifies HTTP URLs through the ag_http_url
@@ -181,7 +171,7 @@ extern ag_string                *ag_http_status_str(enum ag_http_status);
  *   - include/object.h
  *   - src/http-url.c
  *   - test/http-url.c
- */
+ **/
 
 AG_OBJECT_DECLARE(ag_http_url, AG_TYPEID_HTTP_URL);
 
@@ -197,9 +187,7 @@ extern ag_uint           ag_http_url_port(const ag_http_url *ctx);
 extern ag_string        *ag_http_url_path(const ag_http_url *ctx);
 
 
-
-
-/*******************************************************************************
+/**
  * Declare the HTTP client interface. An HTTP client represents the client
  * making the HTTP request. Although the HTTP specification does not explicitly
  * mention a client type, we have introduced it for convenience. The Argent
@@ -228,7 +216,7 @@ extern ag_string        *ag_http_url_path(const ag_http_url *ctx);
  *   - include/object.h
  *   - src/http-client.c
  *   - test/http-client.c
- */
+ **/
 
 AG_OBJECT_DECLARE(ag_http_client, AG_TYPEID_HTTP_CLIENT);
 
@@ -242,9 +230,7 @@ extern ag_string        *ag_http_client_agent(const ag_http_client *);
 extern ag_string        *ag_http_client_referer(const ag_http_client *);
 
 
-
-
-/*******************************************************************************
+/**
  * An HTTP request is one half of the HTTP protocol, representing a message sent
  * by the client to the server for some resource. The Argent Library reifies
  * HTTP requests through the ag_http_request object with the type ID
@@ -269,14 +255,13 @@ extern ag_string        *ag_http_client_referer(const ag_http_client *);
  *   - include/object.h
  *   - src/http-request.c
  *   - test/http-request.c
- */
+ **/
 
 AG_OBJECT_DECLARE(ag_http_request, AG_TYPEID_HTTP_REQUEST);
 
 extern ag_http_request  *ag_http_request_new(enum ag_http_method,
                             enum ag_http_mime, const ag_http_url *,
                             const ag_http_client *, const ag_alist *);
-
 
 extern enum ag_http_mime         ag_http_request_mime(const ag_http_request *);
 extern enum ag_http_method       ag_http_request_method(
@@ -287,9 +272,7 @@ extern ag_http_url              *ag_http_request_url(const ag_http_request *);
 extern ag_alist                 *ag_http_request_param(const ag_http_request *);
 
 
-
-
-/*******************************************************************************
+/**
  * The other half of the HTTP protocol is the response sent by the server to the
  * client. The Argent Library reifies the HTTP response through the object
  * ag_http_response with type ID AG_TYPEID_HTTP_REQUEST.
@@ -311,7 +294,7 @@ extern ag_alist                 *ag_http_request_param(const ag_http_request *);
  *   - include/object.h
  *   - src/http-response.c
  *   - test/http-response.c
- */
+ **/
 
 AG_OBJECT_DECLARE(ag_http_response, AG_TYPEID_HTTP_RESPONSE);
 
@@ -329,8 +312,6 @@ extern void             ag_http_response_add(ag_http_response **, const char *);
 extern void             ag_http_response_add_file(ag_http_response **,
                             const char *);
 extern void             ag_http_response_flush(ag_http_response **);
-
-
 
 
 #ifdef __cplusplus
