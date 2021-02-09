771016a787b34d6ca2d4c6af5cd84fe1e2ff41c7
Abhishek Chakravarti <abhishek@taranjali.org>
2021-02-09 11:20:06 +0530

    Implement ag_http_server_register() and ag_http_server_register_dso()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/http-server.c
+++ src/http-server.c
@@ -4,7 +4,7 @@
 struct registry_node {
         ag_string               *path;
         ag_string               *dso;
-        ag_string               *hnd;
+        ag_string               *sym;
         struct registry_node    *next;
 };
 
@@ -13,39 +13,11 @@ static struct registry_node     *registry_node_new(const char *, const char *,
 static void                      registry_node_release(struct registry_node *);
 
 
-static AG_THREADLOCAL struct registry_node **g_registry = NULL;
+static AG_THREADLOCAL struct registry_node      **g_registry = NULL;
 
-#define REGISTRY_BUCKETS sizeof(int)
+#define REGISTRY_BUCKETS        sizeof(int)
 
-
-static struct registry_node *
-registry_node_new(const char *path, const char *dso, const char *hnd)
-{
-        AG_ASSERT_STR (path);
-        AG_ASSERT_STR (hnd);
-
-        struct registry_node *n = ag_memblock_new(sizeof *n);
-
-        n->path = ag_string_new(path);
-        n->dso = ag_string_new(dso);
-        n->hnd = ag_string_new(hnd);
-        n->next = NULL;
-
-        return n;
-}
-
-static void
-registry_node_release(struct registry_node *ctx)
-{
-        if (AG_LIKELY (ctx)) {
-                ag_string_release(&ctx->path);
-                ag_string_release(&ctx->dso);
-                ag_string_release(&ctx->hnd);
-
-                ag_memblock *hnd = &ctx;
-                ag_memblock_release(hnd);
-        }
-}
+static void     registry_push(struct registry_node *);
 
 
 extern void
@@ -72,5 +44,75 @@ ag_http_server_exit(void)
                         n = n2;
                 }
         }
+}
+
+
+extern void
+ag_http_server_register(const char *path, const char *sym)
+{
+        AG_ASSERT_STR (path);
+        AG_ASSERT_STR (sym);
+        AG_ASSERT_PTR (g_registry);
+
+        registry_push(registry_node_new(path, NULL, sym));
+}
+
+extern void
+ag_http_server_register_dso(const char *path, const char *dso, const char *sym)
+{
+        AG_ASSERT_STR (path);
+        AG_ASSERT_STR (dso);
+        AG_ASSERT_STR (sym);
+        AG_ASSERT_PTR (g_registry);
+
+        registry_push(registry_node_new(path, dso, sym));
+}
+
+
+static struct registry_node *
+registry_node_new(const char *path, const char *dso, const char *sym)
+{
+        AG_ASSERT_STR (path);
+        AG_ASSERT_STR (sym);
+
+        struct registry_node *n = ag_memblock_new(sizeof *n);
+
+        n->path = ag_string_new(path);
+        n->dso = ag_string_new(dso);
+        n->sym = ag_string_new(sym);
+        n->next = NULL;
+
+        return n;
+}
+
+static void
+registry_node_release(struct registry_node *ctx)
+{
+        if (AG_LIKELY (ctx)) {
+                ag_string_release(&ctx->path);
+                ag_string_release(&ctx->dso);
+                ag_string_release(&ctx->sym);
+
+                ag_memblock *hnd = &ctx;
+                ag_memblock_release(hnd);
+        }
+}
+
+
+static void
+registry_push(struct registry_node *node)
+{
+        AG_ASSERT (node);
+
+        ag_hash h = ag_hash_new_str(node->path);
+        register struct registry_node *i = g_registry[h % REGISTRY_BUCKETS];
+
+        if (i) {
+                while (i && i->next)
+                        i = i->next;
+
+                i->next = node;
+        } else
+                i = node;
 }
 
