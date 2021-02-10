2b111a32bb82df14ecdf30d0e481a2c007ba5e4c
Abhishek Chakravarti <abhishek@taranjali.org>
2021-02-10 15:04:36 +0530

    Register plugin object
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/manager.c
+++ src/manager.c
@@ -68,6 +68,7 @@ ag_init(void)
         AG_OBJECT_REGISTER(ag_field);
         AG_OBJECT_REGISTER(ag_list);
         AG_OBJECT_REGISTER(ag_alist);
+        AG_OBJECT_REGISTER(ag_plugin);
         AG_OBJECT_REGISTER(ag_http_url);
         AG_OBJECT_REGISTER(ag_http_client);
         AG_OBJECT_REGISTER(ag_http_request);
