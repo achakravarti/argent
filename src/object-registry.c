a0ead0d72015ffa06fa81232d220d041c5d7b4fa
Abhishek Chakravarti <abhishek@taranjali.org>
2021-02-18 08:00:57 +0530

    Reformat
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/object-registry.c
+++ src/object-registry.c
@@ -24,15 +24,10 @@
 #include "../include/argent.h"
 
 
-static void
-vt_release(void *hnd)
-{
-        ag_memblock_release(&hnd);
-}
-
-
 static ag_registry *g_argent;
 static ag_registry *g_client;
+
+static void     vt_release(void *);
 
 
 /* Prototypes for the default callback functions */
@@ -46,21 +41,24 @@ static size_t       def_hash(const ag_object *);
 static ag_string   *def_str(const ag_object *);
 
 
-extern void ag_object_registry_init(void)
+extern void
+ag_object_registry_init(void)
 {
         g_argent = ag_registry_new(vt_release);
         g_client = ag_registry_new(vt_release);
 }
 
 
-extern void ag_object_registry_exit(void)
+extern void
+ag_object_registry_exit(void)
 {
         ag_registry_release(&g_argent);
         ag_registry_release(&g_client);
 }
 
 
-extern const struct ag_object_vtable *ag_object_registry_get(ag_typeid typeid)
+extern const struct ag_object_vtable *
+ag_object_registry_get(ag_typeid typeid)
 {
         ag_registry *r = typeid < 0 ? g_argent : g_client;
         ag_hash h = ag_hash_new(typeid);
@@ -70,8 +68,8 @@ extern const struct ag_object_vtable *ag_object_registry_get(ag_typeid typeid)
 }
 
 
-extern void ag_object_registry_set(ag_typeid typeid,
-                                const struct ag_object_vtable *vt)
+extern void
+ag_object_registry_set(ag_typeid typeid, const struct ag_object_vtable *vt)
 {
         struct ag_object_vtable *v = ag_memblock_new(sizeof *v);
         v->clone = vt->clone ? vt->clone : def_clone;
@@ -89,7 +87,15 @@ extern void ag_object_registry_set(ag_typeid typeid,
 }
 
 
-static ag_memblock *def_clone(const ag_memblock *ctx)
+static void
+vt_release(void *hnd)
+{
+        ag_memblock_release(&hnd);
+}
+
+
+static ag_memblock *
+def_clone(const ag_memblock *ctx)
 {
         return ag_memblock_clone(ctx);
 }
@@ -97,51 +103,58 @@ static ag_memblock *def_clone(const ag_memblock *ctx)
 
 // we don't do anything because ag_object_release() takes care of releasing the
 // memory allocated to the payload
-static void def_release(ag_memblock *ctx)
+static void
+def_release(ag_memblock *ctx)
 {
         (void)ctx;
 }
 
 
-static enum ag_cmp def_cmp(const ag_object *ctx, const ag_object *cmp)
+static enum ag_cmp
+def_cmp(const ag_object *ctx, const ag_object *cmp)
 {
         return ag_memblock_cmp(ctx, cmp);
 }
 
 
-static bool def_valid(const ag_object *ctx)
+static bool
+def_valid(const ag_object *ctx)
 {
         return ctx;
 }
 
 
-static size_t def_sz(const ag_object *ctx)
+static size_t
+def_sz(const ag_object *ctx)
 {
         return ag_memblock_sz(ctx) + ag_memblock_sz(ag_object_payload(ctx));
 }
 
 
-static size_t def_len(const ag_object *ctx)
+static size_t
+def_len(const ag_object *ctx)
 {
         (void)ctx;
         return 1;
 }
 
 
-static ag_hash def_hash(const ag_object *ctx)
+static ag_hash
+def_hash(const ag_object *ctx)
 {
         AG_AUTO(ag_uuid) *u = ag_object_uuid(ctx);
         return ag_uuid_hash(u);
 }
 
 
-static ag_string *def_str(const ag_object *ctx)
+static ag_string *
+def_str(const ag_object *ctx)
 {
-        AG_AUTO(ag_uuid) *u    = ag_object_uuid(ctx);
+        AG_AUTO(ag_uuid) *u = ag_object_uuid(ctx);
         AG_AUTO(ag_string)  *ustr = ag_uuid_str(u);
         AG_AUTO(ag_string)  *mstr = ag_memblock_str(ctx);
 
-        return ag_string_new_fmt("typeid = %d, uuid = %s, %s", ag_object_typeid(ctx),
-                              ustr, mstr);
+        return ag_string_new_fmt("typeid = %d, uuid = %s, %s",
+            ag_object_typeid(ctx), ustr, mstr);
 }
 
