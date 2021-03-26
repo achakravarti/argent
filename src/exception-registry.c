7c962d380dbb51306ed01ec5a2a195b138f52ab3
Abhishek Chakravarti <abhishek@taranjali.org>
2021-03-26 13:25:00 +0530

    Refactor exception registry implementation
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/exception-registry.c
+++ src/exception-registry.c
@@ -27,66 +27,57 @@
 #include <string.h>
 
 
-struct data {
+/*******************************************************************************
+ * For every exception that we register, we need to keep track of its exception
+ * message and its exception handler. The `ex` struct encapsulates this data,
+ * and is supported by three related functions:
+ *   - `ex_new(), 
+ *   - `ex_release()`, and
+ *   - `ex_default()`.
+ */
+
+struct ex {
         char                    *msg;
         ag_exception_hnd        *hnd;
 };
 
-static void     data_release(void *);
+static struct ex        *ex_new(const char *, ag_exception_hnd *);
+static void              ex_release(void *);
+static void              ex_default(const struct ag_exception *, void *);
 
 
-/*
+/*******************************************************************************
  * Although the exception registry presents itself through its interface as a
- * single list, it is implemented as two separate vectors. One vector maintains
+ * single registry, it is implemented as two separate units. One unit maintains
  * the exception messages and handlers for the Argent Library error codes
- * (negative `ag_erno` values), and once for client code (positive `ag_erno`
+ * (negative `ag_erno` values), and one for client code (positive `ag_erno`
  * values).
  */
+
 static ag_registry      *g_argent = NULL;
 static ag_registry      *g_client = NULL;
 
 
-/* 
- * Prototypes for assertion helpers; we use these functions instead of the
- * predicates they wrap to improve readability. These helper functions are
- * available only in debug builds.
+/*******************************************************************************
+ * `ag_exception_registry_init()` initialises the exception registry vectors 
+ * with a starting capacity.
  */
-#ifndef NDEBUG
-static inline bool      is_exception_registry_initialised(void);
-static inline bool      is_erno_valid(ag_erno);
-static inline bool      is_msg_valid(const char *);
-#endif
 
-
-/* Prototypes for helper functions for managing dynamic memory and strings. */
-static inline void      *mem_new(size_t);
-static inline void       mem_release(void *);
-static inline char      *str_new(const char *);
-static inline void       str_release(char *);
-
-
-/* Prototype for the default exception handler */
-static void     hnd_default(const struct ag_exception *, void *);
-
-
-/* 
- * ag_exception_registry_init() initialises the exception registry vectors with
- * a starting capacity.
- */
 extern void
 ag_exception_registry_init(void)
 {
-        AG_ASSERT (!is_exception_registry_initialised());
+        AG_ASSERT_TAG ("EXCEPTION_REGISTRY_NOT_INIT", !(g_argent && g_client));
 
-        g_argent = ag_registry_new(data_release);
-        g_client = ag_registry_new(data_release);
+        g_argent = ag_registry_new(ex_release);
+        g_client = ag_registry_new(ex_release);
 }
 
 
-/*
- * ag_exception_registry() shuts down the exception registry, releaseing the
+/*******************************************************************************
+ * `ag_exception_registry()` shuts down the exception registry, releasing the
  * heap memory resources used by it.
  */
+
 extern void
 ag_exception_registry_exit(void)
 {
@@ -95,167 +86,127 @@ ag_exception_registry_exit(void)
 }
 
 
-/*
- * ag_exception_registry_msg() gets the exception message associated with a
+/*******************************************************************************
+ * `ag_exception_registry_msg()` gets the exception message associated with a
  * given error code.
  */
+
 extern const char *
 ag_exception_registry_msg(ag_erno erno)
 {
-        AG_ASSERT (is_exception_registry_initialised());
-        AG_ASSERT (is_erno_valid(erno));
+        AG_ASSERT_TAG ("EXCEPTION_REGISTRY_INIT", g_argent && g_client);
+        AG_ASSERT_TAG ("ERNO_VALID", erno);
 
         ag_registry *r = erno < 0 ? g_argent : g_client;
         ag_hash h = ag_hash_new(erno);
 
-        struct data *d = ag_registry_get(r, h);
-        return d->msg;
+        struct ex *x = ag_registry_get(r, h);
+        return x->msg;
 }
 
 
-/*
- * ag_exception_registry_hnd() gets the exception handler associated with a
+/*******************************************************************************
+ * `ag_exception_registry_hnd()` gets the exception handler associated with a
  * given error code.
  */
+
 extern ag_exception_hnd *
 ag_exception_registry_hnd(ag_erno erno)
 {
-        AG_ASSERT (is_exception_registry_initialised());
-        AG_ASSERT (is_erno_valid(erno));
+        AG_ASSERT_TAG ("EXCEPTION_REGISTRY_INIT", g_argent && g_client);
+        AG_ASSERT_TAG ("ERNO_VALID", erno);
 
         ag_registry *r = erno < 0 ? g_argent : g_client;
         ag_hash h = ag_hash_new(erno);
 
-        struct data *d = ag_registry_get(r, h);
-        return d->hnd;
+        struct ex *x = ag_registry_get(r, h);
+        return x->hnd;
 }
 
 
-/*
- * ag_exception_registry_set() sets the exception message and handler associated
- * with a given error code. In case NULL is passed for hnd, then the default
- * exception handler is associated with the error code.
- */
-/*
+/*******************************************************************************
  * The `ag_exception_registry_set()` function is the only mutator for the
  * exception registry, and sets the exception message and handler associated
  * with a given error code. Subsequent calls to this function override the
  * previous values, and passing a NULL pointer for the handler causes the
  * default unhandled exception handler to be set.
  */
+
 extern void
 ag_exception_registry_set(ag_erno erno, const char *msg, ag_exception_hnd *hnd)
 {
-        AG_ASSERT (is_exception_registry_initialised());
-        AG_ASSERT (is_erno_valid(erno));
-        AG_ASSERT (is_msg_valid(msg));
+        AG_ASSERT_TAG ("EXCEPTION_REGISTRY_INIT", g_argent && g_client);
+        AG_ASSERT_TAG ("ERNO_VALID", erno);
+        AG_ASSERT_STR (msg);
 
-        struct data *d = mem_new(sizeof *d);
-        d->msg = str_new(msg);
-        d->hnd = hnd ? hnd : hnd_default;
+        struct ex *x = ex_new(msg, hnd);
 
         ag_registry *r = erno < 0 ? g_argent : g_client;
         ag_hash h = ag_hash_new(erno);
-        ag_registry_push(r, h, d);
+        ag_registry_push(r, h, x);
 }
 
 
-/* 
- * is_exception_registry_initialised() checks if the vectors for both the Argent
- * Library and client code have been initialised. This function is required only
- * for debug builds.
+/*******************************************************************************
+ * `ex_new()` creates a new instance of an `ex` struct with a given exception
+ * message and handler. In case a null pointer is passed for the second
+ * parameter, then the default exception handler will be used. We avoid the use
+ * of the `ag_memblock` and `ag_string` interfaces in order to prevent potential
+ * circular dependencies.
  */
-#ifndef NDEBUG
-static inline bool
-is_exception_registry_initialised(void)
+
+static struct ex *
+ex_new(const char *msg, ag_exception_hnd *hnd)
 {
-        return g_argent && g_client;
+        AG_ASSERT_STR (msg);
+
+        struct ex *x = malloc(sizeof *x);
+
+        if (AG_UNLIKELY (!x))
+                goto error;
+
+        size_t sz = strlen(msg);
+        x->msg = malloc(sz + 1);
+
+        if (AG_UNLIKELY (!x->msg))
+                goto error;
+
+        strcpy(x->msg, msg);
+        x->msg[sz] = '\0';
+
+        x->hnd = hnd ? hnd : ex_default;
+        return x;
+
+error:
+        printf("[!] failed to allocate memory for exception registry\n");
+        exit(EXIT_FAILURE);
 }
-#endif
 
-
-#ifndef NDEBUG
-static inline bool
-is_erno_valid(ag_erno erno)
-{
-        return erno;
-}
-#endif
-
-
-/*
- * is_msg_valid() checks whether a given string is valid for use as an exception
- * message, i.e. it is not an empty string.
+       
+/*******************************************************************************
+ * `ex_release()` frees up the heap memory resources used by an `ex` struct
+ * instance.
  */
-#ifndef NDEBUG
-static inline bool
-is_msg_valid(const char *msg)
+
+static void
+ex_release(void *hnd)
 {
-        return msg && *msg;
-}
-#endif
-
-
-/* 
- * mem_new() allocates a new block of heap memory of a given size, and fills the
- * block with 0.
- */
-static inline void *
-mem_new(size_t sz)
-{
-        void *ctx = malloc(sz);
-
-        if (AG_UNLIKELY (!ctx)) {
-                printf("[!] failed to allocate memory for exception "
-                    "registry\n");
-                exit(EXIT_FAILURE);
-        }
-
-        memset(ctx, 0, sz);
-
-        return ctx;
-}
-
-
-static inline void
-mem_release(void *hnd)
-{
-        if (AG_LIKELY (hnd))
+        if (AG_LIKELY (hnd)) {
+                free(((struct ex *)hnd)->msg);
                 free(hnd);
-}
-
-        
-/* str_new() creates a new dynamic string from a given static string. */
-static inline char *
-str_new(const char *src)
-{
-        size_t sz = strlen(src);
-
-        char *ctx = mem_new(sz + 1);
-        strncpy(ctx, src, sz);
-        ctx[sz] = '\0';
-        
-        return ctx;
+        }
 }
 
 
-/* str_dispose() release a dynamic string created with str_new(). */
-static inline void
-str_release(char *ctx)
-{
-        mem_release(ctx);
-}
-
-
-/*
- * hnd_default() is the default exception handler used in case client code
- * passes a NULL exception handler through ag_exception_reistry_set(). This
+/*******************************************************************************
+ * `ex_default()` is the default exception handler used in case client code
+ * passes a NULL exception handler through `ag_exception_reistry_set()`. This
  * handler simply terminates the application after printing and logging the
  * exception metadata.
  */
 
 static void
-hnd_default(const struct ag_exception *ex, void *opt)
+ex_default(const struct ag_exception *ex, void *opt)
 {
         (void) opt;
     
@@ -266,13 +217,5 @@ hnd_default(const struct ag_exception *ex, void *opt)
             ex->file, ex->line, ag_exception_registry_msg(ex->erno));
 
         ag_exit(EXIT_FAILURE);
-}
-
-
-static void
-data_release(void *hnd)
-{
-        str_release(((struct data *)hnd)->msg);
-        mem_release(hnd);
 }
 
