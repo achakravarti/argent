ca80453e11a9347b5cbe9483b6e53faa82d19a0e
Abhishek Chakravarti <abhishek@taranjali.org>
2021-04-01 11:21:51 +0530

    Refactor layout
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- include/object.h
+++ include/object.h
@@ -38,69 +38,6 @@ extern "C" {
 
 typedef struct ag_object ag_object;
 
-typedef ag_memblock *(ag_object_clone_virt)(const ag_memblock *);
-typedef void         (ag_object_release_virt)(ag_memblock *);
-typedef enum ag_cmp  (ag_object_cmp_virt)(const ag_object *, const ag_object *);
-typedef bool         (ag_object_valid_virt)(const ag_object *);
-typedef size_t       (ag_object_sz_virt)(const ag_object *);
-typedef size_t       (ag_object_len_virt)(const ag_object *);
-typedef ag_hash      (ag_object_hash_virt)(const ag_object *);
-typedef ag_string   *(ag_object_str_virt)(const ag_object *);
-
-
-struct ag_object_vtable {
-        ag_object_clone_virt   *clone;   /* Clone callback              */
-        ag_object_release_virt *release; /* Release callback            */
-        ag_object_cmp_virt     *cmp;     /* Comparison callback         */
-        ag_object_valid_virt   *valid;   /* Validation callback         */
-        ag_object_sz_virt      *sz;      /* Size computation callback   */
-        ag_object_len_virt     *len;     /* Length computation callback */
-        ag_object_hash_virt    *hash;    /* Hash computation callback   */
-        ag_object_str_virt     *str;     /* String generation callback  */
-};
-
-
-extern ag_object *ag_object_new(ag_typeid, ag_memblock *);
-extern ag_object *ag_object_copy(const ag_object *);
-extern ag_object *ag_object_clone(const ag_object *);
-extern void ag_object_release(ag_object **);
-
-
-extern enum ag_cmp ag_object_cmp(const ag_object *, const ag_object *);
-
-inline bool ag_object_lt(const ag_object *ctx, const ag_object *cmp)
-{
-        return ag_object_cmp(ctx, cmp) == AG_CMP_LT;
-}
-
-inline bool ag_object_eq(const ag_object *ctx, const ag_object *cmp)
-{
-        return ag_object_cmp(ctx, cmp) == AG_CMP_EQ;
-}
-
-inline bool ag_object_gt(const ag_object *ctx, const ag_object *cmp)
-{
-        return ag_object_cmp(ctx, cmp) == AG_CMP_GT;
-}
-
-
-extern ag_typeid ag_object_typeid(const ag_object *);
-extern ag_uuid *ag_object_uuid(const ag_object *);
-extern bool ag_object_valid(const ag_object *);
-extern size_t ag_object_sz(const ag_object *);
-extern size_t ag_object_refc(const ag_object *);
-extern size_t ag_object_len(const ag_object *);
-extern ag_hash ag_object_hash(const ag_object *);
-extern ag_string *ag_object_str(const ag_object *);
-extern const ag_memblock *ag_object_payload(const ag_object *);
-extern ag_memblock *ag_object_payload_mutable(ag_object **);
-
-inline bool ag_object_empty(const ag_object *ctx)
-{
-        return !ag_object_len(ctx);
-}
-
-
 #define AG_OBJECT_DECLARE(name, typeid)                                        \
         typedef ag_object name;                                                \
         inline name *name ## _copy(const name *ctx)                            \
@@ -205,7 +142,6 @@ inline bool ag_object_empty(const ag_object *ctx)
         }                                                                      \
         extern void __ ## name ## _register__(void)
 
-
 #define AG_OBJECT_DEFINE(name)                                                 \
         extern inline name *name ## _copy(const name *);                       \
         extern inline name *name ## _clone(const name *);                      \
@@ -225,15 +161,77 @@ inline bool ag_object_empty(const ag_object *ctx)
         extern inline ag_string *name ## _str(const name *);                   \
         extern void __ ## name ## _register__(void)
 
-
 #define AG_OBJECT_REGISTER(name) __ ## name ##_register__()
 
+extern ag_object                *ag_object_new(ag_typeid, ag_memblock *);
+extern ag_object                *ag_object_copy(const ag_object *);
+extern ag_object                *ag_object_clone(const ag_object *);
+extern void                      ag_object_release(ag_object **);
+extern enum ag_cmp               ag_object_cmp(const ag_object *,
+                                    const ag_object *);
+extern ag_typeid                 ag_object_typeid(const ag_object *);
+extern ag_uuid                  *ag_object_uuid(const ag_object *);
+extern bool                      ag_object_valid(const ag_object *);
+extern size_t                    ag_object_sz(const ag_object *);
+extern size_t                    ag_object_refc(const ag_object *);
+extern size_t                    ag_object_len(const ag_object *);
+extern ag_hash                   ag_object_hash(const ag_object *);
+extern ag_string                *ag_object_str(const ag_object *);
+extern const ag_memblock        *ag_object_payload(const ag_object *);
+extern ag_memblock              *ag_object_payload_mutable(ag_object **);
 
-extern void ag_object_registry_init(void);
-extern void ag_object_registry_exit(void);
+inline bool
+ag_object_lt(const ag_object *ctx, const ag_object *cmp)
+{
+        return ag_object_cmp(ctx, cmp) == AG_CMP_LT;
+}
 
-extern const struct ag_object_vtable *ag_object_registry_get(ag_typeid);
-extern void ag_object_registry_push(ag_typeid, const struct ag_object_vtable *);
+inline bool
+ag_object_eq(const ag_object *ctx, const ag_object *cmp)
+{
+        return ag_object_cmp(ctx, cmp) == AG_CMP_EQ;
+}
+
+inline bool
+ag_object_gt(const ag_object *ctx, const ag_object *cmp)
+{
+        return ag_object_cmp(ctx, cmp) == AG_CMP_GT;
+}
+
+inline bool
+ag_object_empty(const ag_object *ctx)
+{
+        return !ag_object_len(ctx);
+}
+
+
+typedef ag_memblock *(ag_object_clone_virt)(const ag_memblock *);
+typedef void         (ag_object_release_virt)(ag_memblock *);
+typedef enum ag_cmp  (ag_object_cmp_virt)(const ag_object *, const ag_object *);
+typedef bool         (ag_object_valid_virt)(const ag_object *);
+typedef size_t       (ag_object_sz_virt)(const ag_object *);
+typedef size_t       (ag_object_len_virt)(const ag_object *);
+typedef ag_hash      (ag_object_hash_virt)(const ag_object *);
+typedef ag_string   *(ag_object_str_virt)(const ag_object *);
+
+
+struct ag_object_vtable {
+        ag_object_clone_virt   *clone;   /* Clone callback              */
+        ag_object_release_virt *release; /* Release callback            */
+        ag_object_cmp_virt     *cmp;     /* Comparison callback         */
+        ag_object_valid_virt   *valid;   /* Validation callback         */
+        ag_object_sz_virt      *sz;      /* Size computation callback   */
+        ag_object_len_virt     *len;     /* Length computation callback */
+        ag_object_hash_virt    *hash;    /* Hash computation callback   */
+        ag_object_str_virt     *str;     /* String generation callback  */
+};
+
+
+extern void                              ag_object_registry_init(void);
+extern void                              ag_object_registry_exit(void);
+extern const struct ag_object_vtable    *ag_object_registry_get(ag_typeid);
+extern void                              ag_object_registry_push(ag_typeid,
+                                            const struct ag_object_vtable *);
 
 
 #ifdef __cplusplus
