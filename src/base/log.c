46561a0b71e1bd561732eb94e98a49be9040174e
Abhishek Chakravarti <abhishek@taranjali.org>
2021-04-25 09:17:15 +0530

    Add implementation comments for the logging unit
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/base/log.c
+++ src/base/log.c
@@ -23,12 +23,16 @@
 
 #include "../argent.h"
 
-#include <syslog.h>
 #include <stdarg.h>
+#include <syslog.h>
 
 
 /*******************************************************************************
- *
+ * `LOG_WRITE()` is a helper macro for the `ag_log_write()` wrapper functions.
+ * Since these wrapper functions are semantically similar except for the log
+ * priority level, `LOG_WRITE()` abstracts the code that is common to them.
+ * We've used a macro instead of a helper function in order to avoid the
+ * complication of passing around variable argument lists.
  */
 
 #define LOG_WRITE(FMT, LVL)             \
@@ -40,13 +44,17 @@
 
 
 /*******************************************************************************
- *
+ * `ag_log_write()` writes a formatted message to the system log with a given
+ * priority level. The priority level is passed through the first parameter, the
+ * message through the second, and the format specifiers through the variable
+ * argument list. The format specifiers are rquired only if the message is
+ * formatted.
  */
 
 extern void 
-ag_log_write(enum ag_log_level lvl, const char *fmt, ...)
+ag_log_write(enum ag_log_level lvl, const char *msg, ...)
 {
-        AG_ASSERT_STR (fmt);
+        AG_ASSERT_STR (msg);
 
         int map[] = {
                 LOG_EMERG,
@@ -62,71 +70,86 @@ ag_log_write(enum ag_log_level lvl, const char *fmt, ...)
         openlog(NULL, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_USER);
 
         va_list ap;
-        va_start(ap, fmt);
-        vsyslog(map[lvl], fmt, ap);
+        va_start(ap, msg);
+        vsyslog(map[lvl], msg, ap);
         va_end(ap);
 
         closelog();
 }
 
+
 /*******************************************************************************
- *
+ * `ag_log_emerg()` is a convenience wrapper around `ag_log_write()` that logs a
+ * formatted emergency message to the system log. The message is passed through
+ * the first parameter, and the format specifiers are passed through the
+ * variable argument list. The format specifiers are required only if the
+ * message is formatted.
  */
 
 extern void
-ag_log_emerg(const char *fmt, ...)
+ag_log_emerg(const char *msg, ...)
 {
-        LOG_WRITE(fmt, AG_LOG_LEVEL_EMERG);
+        LOG_WRITE(msg, AG_LOG_LEVEL_EMERG);
 }
 
 
 /*******************************************************************************
- *
+ * `ag_log_alert()` is a convenience wraapper around `ag_log_write()` that logs
+ * a formatted alert message to the system log. The parameters are semantically
+ * the same as `ag_log_emerg()`.
  */
 
 extern void
-ag_log_alert(const char *fmt, ...)
+ag_log_alert(const char *msg, ...)
 {
-        LOG_WRITE(fmt, AG_LOG_LEVEL_ALERT);
+        LOG_WRITE(msg, AG_LOG_LEVEL_ALERT);
 }
 
 
 /*******************************************************************************
- *
+ * `ag_log_crit()` is a convenience wrapper around `ag_log_write()` that logs a
+ * formatted critical message to the system log. The parameters are semantically
+ * the same as `ag_log_emerg()`.
  */
 
 extern void
-ag_log_crit(const char *fmt, ...)
+ag_log_crit(const char *msg, ...)
 {
-        LOG_WRITE(fmt, AG_LOG_LEVEL_CRIT);
+        LOG_WRITE(msg, AG_LOG_LEVEL_CRIT);
 
 }
 
 
 /*******************************************************************************
- *
+ * `ag_log_err()` is a convenience wrapper aournd `ag_log_write()` that logs a
+ * formatted error message to the system log. The parameters are semantically
+ * the same as `ag_log_emerg()`.
  */
 
 extern void
-ag_log_err(const char *fmt, ...)
+ag_log_err(const char *msg, ...)
 {
-        LOG_WRITE(fmt, AG_LOG_LEVEL_ERR);
+        LOG_WRITE(msg, AG_LOG_LEVEL_ERR);
 }
 
 
 /*******************************************************************************
- *
+ * `ag_log_warning()` is a convenience wrapper around `ag_log_write()` that logs
+ * a formatted warning message to the system log. The parameters are
+ * semantically the same as `ag_log_emerg()`.
  */
 
 extern void
-ag_log_warning(const char *fmt, ...)
+ag_log_warning(const char *msg, ...)
 {
-        LOG_WRITE(fmt, AG_LOG_LEVEL_WARNING);
+        LOG_WRITE(msg, AG_LOG_LEVEL_WARNING);
 }
 
 
 /*******************************************************************************
- *
+ * `ag_log_notice()` is a convenience wrapper around `ag_log_write()` that logs
+ * a formatted notice message to the system log. The parameters are semantically
+ * the same as `ag_log_emerg()`.
  */
 
 extern void 
@@ -137,23 +160,27 @@ ag_log_notice(const char *fmt, ...)
 
 
 /*******************************************************************************
- *
+ * `ag_log_info()` is a convenience wrapper around `ag_log_write()` that logs a
+ * formatted information message to the system log. The parameters are
+ * semantically the same as `ag_log_emerg()`.
  */
 
 extern void
-ag_log_info(const char *fmt, ...)
+ag_log_info(const char *msg, ...)
 {
-        LOG_WRITE(fmt, AG_LOG_LEVEL_INFO);
+        LOG_WRITE(msg, AG_LOG_LEVEL_INFO);
 }
 
 
 /*******************************************************************************
- *
+ * `ag_log_debug()` is a convenience wrapper around `ag_log_write()` that logs a
+ * formatted debug message to the system log. The parameters are semantically
+ * the same as `ag_log_emerg()`.
  */
 
 extern void
-ag_log_debug(const char *fmt, ...)
+ag_log_debug(const char *msg, ...)
 {
-        LOG_WRITE(fmt, AG_LOG_LEVEL_DEBUG);
+        LOG_WRITE(msg, AG_LOG_LEVEL_DEBUG);
 }
 
