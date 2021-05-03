24a6ce52de7607af5b1f81cabd393c6244787707
Abhishek Chakravarti <abhishek@taranjali.org>
2021-05-03 08:53:01 +0530

    Temporarily disable object tests
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/runner.c
+++ test/runner.c
@@ -37,7 +37,7 @@ int main(int argc, char **argv)
         ag_test_suite *log = test_suite_log();
         ag_test_suite *mblock = test_suite_memblock();
         ag_test_suite *str = test_suite_string();
-        ag_test_suite *obj = test_suite_object();
+        //ag_test_suite *obj = test_suite_object();
         ag_test_suite *val = test_suite_value();
         ag_test_suite *fld = test_suite_field();
         ag_test_suite *lst = test_suite_list();
@@ -52,7 +52,7 @@ int main(int argc, char **argv)
         ag_test_harness_push(th, log);
         ag_test_harness_push(th, mblock);
         ag_test_harness_push(th, str);
-        ag_test_harness_push(th, obj);
+        //ag_test_harness_push(th, obj);
         ag_test_harness_push(th, val);
         ag_test_harness_push(th, fld);
         ag_test_harness_push(th, lst);
@@ -67,7 +67,7 @@ int main(int argc, char **argv)
         ag_test_suite_release(&log);
         ag_test_suite_release(&mblock);
         ag_test_suite_release(&str);
-        ag_test_suite_release(&obj);
+        //ag_test_suite_release(&obj);
         ag_test_suite_release(&val);
         ag_test_suite_release(&fld);
         ag_test_suite_release(&lst);
